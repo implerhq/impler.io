dd8f4c8bb6ea55d9e1941bbe326d572b9066fbee
chavda-bhavik <bhavikvchavda@gmail.com>
2022-10-13 14:38:09 +0530

    feat: Deleted old validator and updated invalid message for new one

--- apps/api/src/app/shared/constants.ts
+++ apps/api/src/app/shared/constants.ts
@@ -2,4 +2,5 @@ export const APIMessages = {
   FILE_TYPE_NOT_VALID: 'File type is not supported.',
   FILE_IS_EMPTY: 'File is empty',
   EMPTY_HEADING_PREFIX: 'Empty Heading',
+  INVALID_TEMPLATE_ID_CODE_SUFFIX: 'is not valid TemplateId or CODE.',
 };
--- apps/api/src/app/shared/validations/valid-template.validation.ts
+++ apps/api/src/app/shared/validations/valid-template.validation.ts
@@ -1,5 +1,6 @@
 import { CommonRepository, TemplateRepository } from '@impler/dal';
 import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { APIMessages } from '../constants';
 
 @Injectable()
 export class ValidateTemplate implements PipeTransform<string> {
@@ -8,7 +9,7 @@ export class ValidateTemplate implements PipeTransform<string> {
   async transform(value: string): Promise<string> {
     const isMongoId = this.commonRepository.validMongoId(value);
     const template = await this.templateRepository.findOne(isMongoId ? { _id: value } : { code: value });
-    if (!template) throw new BadRequestException();
+    if (!template) throw new BadRequestException(`${value} ${APIMessages.INVALID_TEMPLATE_ID_CODE_SUFFIX}`);
 
     return template._id;
   }
