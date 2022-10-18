ca753e76e1bd7cd1d1960375b40ce05a38ae1489
chavda-bhavik <bhavikvchavda@gmail.com>
2022-10-18 11:04:10 +0530

    feat: Added confirm review API

--- apps/api/src/app/review/usecases/index.ts
+++ apps/api/src/app/review/usecases/index.ts
@@ -2,9 +2,13 @@ import { DoReview } from './do-review/do-review.usecase';
 import { SaveReviewData } from './save-review-data/save-review-data.usecase';
 import { GetUploadInvalidData } from './get-upload-invalid-data/get-upload-invalid-data.usecase';
 import { GetFileInvalidData } from './get-file-invalid-data/get-file-invalid-data.usecase';
+import { GetUpload } from '../../shared/usecases/get-upload/get-upload.usecase';
+import { ConfirmReview } from './confirm-review/confirm-review.usecase';
 
 export const USE_CASES = [
   DoReview,
+  GetUpload,
+  ConfirmReview,
   SaveReviewData,
   GetFileInvalidData,
   GetUploadInvalidData,
