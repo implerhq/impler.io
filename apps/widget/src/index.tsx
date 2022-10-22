efefdcef1e88eeb59ffee5ef92581716a1ac3188
chavda-bhavik <bhavikvchavda@gmail.com>
2022-10-22 11:00:21 +0530

    feat: Added widget App

---
+++ apps/widget/src/index.tsx
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { App } from './components/App';
+
+const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+root.render(<App />);
+
+/*
+ * If you want to start measuring performance in your app, pass a function
+ * to log results (for example: reportWebVitals(console.log))
+ * or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ */
+reportWebVitals();
