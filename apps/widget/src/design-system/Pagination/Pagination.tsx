2257314a4378af2fe73c96d84da10628b8f2356f
chavda-bhavik <bhavikvchavda@gmail.com>
2022-11-03 11:54:12 +0530

    feat: Removed total data showing from Pagination

--- apps/widget/src/design-system/Pagination/Pagination.tsx
+++ apps/widget/src/design-system/Pagination/Pagination.tsx
@@ -1,23 +1,17 @@
-import { Pagination as MantinePagination, Group, Text } from '@mantine/core';
+import { Pagination as MantinePagination, Group } from '@mantine/core';
 
 interface IPaginationProps {
   page?: number;
   total: number;
-  limit: number;
-  totalRecords: number;
   size?: 'sm' | 'md';
   onChange?: (page: number) => void;
 }
 
 export function Pagination(props: IPaginationProps) {
-  const { total, page = 1, limit, totalRecords, size = 'md', onChange } = props;
+  const { total, page = 1, size = 'md', onChange } = props;
 
   return (
-    <Group style={{ justifyContent: 'space-between' }}>
-      <Text size={size}>
-        {Math.max(0, Math.min((page - 1) * limit, totalRecords))}-{Math.max(0, Math.min(page * limit, totalRecords))} of{' '}
-        {totalRecords}
-      </Text>
+    <Group style={{ justifyContent: 'center' }}>
       <MantinePagination total={total} page={page} size={size} onChange={onChange} />
     </Group>
   );
