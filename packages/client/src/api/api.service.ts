d61f396da7f1eef5fb82f1d3e7dfca1f28d773e8
chavda-bhavik <bhavikvchavda@gmail.com>
2022-11-08 15:41:07 +0530

    feat: Added APIs for UploadFile and GetTemplates

--- packages/client/src/api/api.service.ts
+++ packages/client/src/api/api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@impler/shared';
+import { HttpClient, ITemplate } from '@impler/shared';
 
 export class ApiService {
   private httpClient: HttpClient;
@@ -23,5 +23,28 @@ export class ApiService {
 
   async checkIsRequestvalid(projectId: string, template?: string) {
     return this.httpClient.post(`/common/valid`, { projectId, template });
+  }
+
+  async uploadFile(data: {
+    template: string;
+    file: File;
+    authHeaderValue?: string;
+    extra?: string;
+  }) {
+    const formData = new FormData();
+    formData.append('file', data.file);
+    if (data.authHeaderValue)
+      formData.append('authHeaderValue', data.authHeaderValue);
+    if (data.extra) formData.append('extra', data.extra);
+
+    return this.httpClient.post(`/upload/${data.template}`, formData, {
+      'Content-Type': 'multipart/form-data',
+    });
+  }
+
+  async getTemplates(projectId: string): Promise<ITemplate[]> {
+    return this.httpClient.get(`/template/${projectId}`) as Promise<
+      ITemplate[]
+    >;
   }
 }
