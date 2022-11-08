a7a86076ebdbaa84793feb139f347370e0a52a41
chavda-bhavik <bhavikvchavda@gmail.com>
2022-11-08 15:47:14 +0530

    feat: Added Loading Indicators to Footer Buttons

--- apps/widget/src/components/Common/Footer/Footer.tsx
+++ apps/widget/src/components/Common/Footer/Footer.tsx
@@ -4,33 +4,43 @@ import { TEXTS } from '@config';
 
 interface IFooterProps {
   active: number;
+  primaryButtonLoading?: boolean;
+  secondaryButtonLoading?: boolean;
   onPrevClick: () => void;
   onNextClick: () => void;
 }
 
 export function Footer(props: IFooterProps) {
-  const { active, onNextClick, onPrevClick } = props;
+  const { active, onNextClick, onPrevClick, primaryButtonLoading, secondaryButtonLoading } = props;
 
   return (
     <Group spacing="md" style={{ alignSelf: 'flex-end' }}>
       {active === 1 ? (
-        <Button onClick={onNextClick}>{TEXTS.PHASE1.SEE_MAPPING}</Button>
+        <Button loading={primaryButtonLoading} onClick={onNextClick}>
+          {TEXTS.PHASE1.SEE_MAPPING}
+        </Button>
       ) : active === 2 ? (
         <>
-          <Button onClick={onPrevClick} variant="outline">
+          <Button loading={secondaryButtonLoading} onClick={onPrevClick} variant="outline">
             {TEXTS.PHASE2.UPLOAD_AGAIN}
           </Button>
-          <Button onClick={onNextClick}>{TEXTS.PHASE2.SEE_REVIEW}</Button>
+          <Button loading={primaryButtonLoading} onClick={onNextClick}>
+            {TEXTS.PHASE2.SEE_REVIEW}
+          </Button>
         </>
       ) : active === 3 ? (
         <>
-          <Button onClick={onPrevClick} variant="outline">
+          <Button loading={secondaryButtonLoading} onClick={onPrevClick} variant="outline">
             {TEXTS.PHASE3.BACK_TO_MAPPING}
           </Button>
-          <Button onClick={onNextClick}>{TEXTS.PHASE3.CONFIRM_UPLOAD}</Button>
+          <Button loading={primaryButtonLoading} onClick={onNextClick}>
+            {TEXTS.PHASE3.CONFIRM_UPLOAD}
+          </Button>
         </>
       ) : active === 4 ? (
-        <Button onClick={onNextClick}>{TEXTS.COMPLETE.UPLOAD_AGAIN}</Button>
+        <Button loading={primaryButtonLoading} onClick={onNextClick}>
+          {TEXTS.COMPLETE.UPLOAD_AGAIN}
+        </Button>
       ) : null}
     </Group>
   );
