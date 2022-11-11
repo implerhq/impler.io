589965e18a97e386b1c08fad73a3781e29cfc437
Chavda Bhavik <bhavikvchavda@gmail.com>
2022-11-11 17:34:22 +0530

    Setup widget demo app (#57)
    * feat: Moved widget-demo to app
    
    * feat: Updated env setup script to setup env for widget-demo app

---
+++ apps/widget-demo/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@impler/react';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Button
+        projectId={import.meta.env.VITE_PROJECT_ID}
+        accessToken={import.meta.env.VITE_ACCESS_TOKEN}
+        template={import.meta.env.VITE_TEMPLATE}
+      />
+    </div>
+  );
+};
