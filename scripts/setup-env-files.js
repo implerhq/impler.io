decbc724c9f3bd198cbc85fb158ec935ca10ad69
Chavda Bhavik <bhavikvchavda@gmail.com>
2022-11-11 11:24:39 +0530

    Lerna Setup (#56)
    * fix: Removed unwanted variables from queue-manager env files
    
    * feat: Added setup-project-env script
    
    * feat: Added learna
    
    * v0.1.0
    
    * feat: Added nvmrc
    
    * feat: Fix clean & prebuild scripts

---
+++ scripts/setup-env-files.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+const fs = require('fs');
+
+(async () => {
+  const apps = ['api', 'queue-manager'];
+
+  console.log('----------------------------------------');
+  console.log('Pre-populating .env files from .example.env');
+
+  for (const app of apps) {
+    const exists = fs.existsSync(`${__dirname}/../apps/${app}/src/.env`);
+
+    if (!exists) {
+      console.log(`Populating ${app} with .env file`);
+      fs.copyFileSync(`${__dirname}/../apps/${app}/src/.example.env`, `${__dirname}/../apps/${app}/src/.env`);
+    }
+  }
+
+  console.log('Finished populating .env files');
+  console.log('----------------------------------------');
+})();
