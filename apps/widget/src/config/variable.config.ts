e32631f63ffc5e4012619544fcac9bee83741718
chavda-bhavik <bhavikvchavda@gmail.com>
2022-12-07 13:20:47 +0530

    feat: Updated dropzone color to change with theme

--- apps/widget/src/config/variable.config.ts
+++ apps/widget/src/config/variable.config.ts
@@ -6,4 +6,8 @@ const milliseconds = 1000,
 export const variables = {
   error: 'message',
   twentyFourHoursInMs: milliseconds * minutes * seconds * hours,
+  colorIndex: 6,
+  baseIndex: 0,
+  firstIndex: 1,
+  hexNumber: 16,
 };
