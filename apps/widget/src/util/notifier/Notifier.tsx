82cdb1946570fe558736f5fe5a5750bfa426d802
chavda-bhavik <bhavikvchavda@gmail.com>
2022-12-25 15:14:30 +0530

    fix: Issue in flow for template selection and traced event using sentry

--- apps/widget/src/util/notifier/Notifier.tsx
+++ apps/widget/src/util/notifier/Notifier.tsx
@@ -1,6 +1,8 @@
-import { colors, TEXTS } from '@config';
+import { colors, TEXTS, ENV, SENTRY_DSN } from '@config';
 import { NotificationContent } from '@types';
 import { showNotification } from '@mantine/notifications';
+import { captureMessage } from '@sentry/react';
+import { ENVTypesEnum } from '@impler/shared';
 
 const autoCloseDuration = 5000;
 export function showError(data: NotificationContent | keyof typeof TEXTS.NOTIFICATIONS) {
@@ -26,4 +28,5 @@ export function showError(data: NotificationContent | keyof typeof TEXTS.NOTIFIC
       },
     }),
   });
+  if (ENV === ENVTypesEnum.PROD && SENTRY_DSN) captureMessage(typeof data === 'string' ? data : data.message);
 }
