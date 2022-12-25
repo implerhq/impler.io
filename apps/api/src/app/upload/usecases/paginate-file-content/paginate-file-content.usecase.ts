91c5c6f59b67d9d0350258a57a1ca8e3709fc584
chavda-bhavik <bhavikvchavda@gmail.com>
2022-12-25 20:39:59 +0530

    feat: Added get uploaded file data API

---
+++ apps/api/src/app/upload/usecases/paginate-file-content/paginate-file-content.usecase.ts
@@ -0,0 +1,15 @@
+import { FileEncodingsEnum, StorageService } from '@impler/shared';
+import { Injectable } from '@nestjs/common';
+import { paginateRecords } from '@shared/helpers/common.helper';
+
+@Injectable()
+export class PaginateFileContent {
+  constructor(private storageService: StorageService) {}
+
+  async execute(path: string, page?: number, limit?: number) {
+    const fileContent: string = await this.storageService.getFileContent(path, FileEncodingsEnum.JSON);
+    const fileData = JSON.parse(fileContent);
+
+    return paginateRecords(fileData, page, limit);
+  }
+}
