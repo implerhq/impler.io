36802924334336ee00c434c20d84d5cb1af1c5bc
chavda-bhavik <bhavikvchavda@gmail.com>
2022-12-29 10:17:53 +0530

    feat: Made Phase4 title to follow primary-color

--- apps/widget/src/components/widget/Phases/Phase4/Phase4.tsx
+++ apps/widget/src/components/widget/Phases/Phase4/Phase4.tsx
@@ -1,10 +1,11 @@
 import { Group, Title, Text } from '@mantine/core';
-import { colors, TEXTS } from '@config';
+import { TEXTS } from '@config';
 import { CheckIcon } from '@icons';
 import { Footer } from 'components/Common/Footer';
 import useStyles from './Styles';
 import { PhasesEum } from '@types';
 import { numberFormatter, replaceVariablesInString } from '@impler/shared';
+import { useAppState } from '@store/app.context';
 
 interface IPhase4Props {
   onCloseClick: () => void;
@@ -14,13 +15,14 @@ interface IPhase4Props {
 
 export function Phase4(props: IPhase4Props) {
   const { classes } = useStyles();
+  const { primaryColor } = useAppState();
   const { rowsCount, onUploadAgainClick, onCloseClick } = props;
 
   return (
     <>
       <Group className={classes.wrapper}>
         <CheckIcon className={classes.check} />
-        <Title color={colors.primary} order={2} mt="md">
+        <Title color={primaryColor} order={2} mt="md">
           {replaceVariablesInString(TEXTS.COMPLETE.title, { count: numberFormatter(rowsCount) })}
         </Title>
         <Text size="xl" color="dimmed">
