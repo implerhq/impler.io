2c6362c6bbee82e116cf13ff46d0980eba338fe9
chavda-bhavik <bhavikvchavda@gmail.com>
2023-01-10 14:32:10 +0530

    feat: Added noopener noreferrer to impler

--- apps/widget/src/components/Common/Footer/Footer.tsx
+++ apps/widget/src/components/Common/Footer/Footer.tsx
@@ -81,7 +81,7 @@ export function Footer(props: IFooterProps) {
 
   return (
     <Group className={classes.wrapper}>
-      <a className={classes.poweredBy} href={variables.implerWebsite} target="_blank">
+      <a className={classes.poweredBy} href={variables.implerWebsite} target="_blank" rel="noopener noreferrer">
         <Text size="xs">
           Powered by <img src="/logo-full.png" className={classes.implerImage} />
         </Text>
