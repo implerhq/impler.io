77dcded64531b30e56533387666b35c6da8a07b8
chavda-bhavik <bhavikvchavda@gmail.com>
2023-04-10 17:31:18 +0530

    feat: Added Card component

---
+++ apps/web/design-system/Card/Card.tsx
@@ -0,0 +1,23 @@
+import { Text } from '@mantine/core';
+import useStyles from './Card.styles';
+
+export type Colors = 'default' | 'primary';
+
+interface CardProps {
+  title: string;
+  subtitle: string;
+  color?: Colors;
+}
+
+export function Card({ title, subtitle, color = 'default' }: CardProps) {
+  const { classes } = useStyles({ color });
+
+  return (
+    <div className={classes.root}>
+      <Text size="md">{title}</Text>
+      <Text size="xl" className={classes.subtitle}>
+        {subtitle}
+      </Text>
+    </div>
+  );
+}
