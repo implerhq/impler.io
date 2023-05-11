e40b7c9967e025f8bf7f115253d27d08a3136c85
chavda-bhavik <bhavikvchavda@gmail.com>
2023-05-11 16:35:12 +0530

    feat: Brought env variables to publicRuntimeConfig

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    NEXT_PUBLIC_EMBED_URL: process.env.NEXT_PUBLIC_EMBED_URL,
+    NEXT_PUBLIC_AMPLITUDE_ID: process.env.NEXT_PUBLIC_AMPLITUDE_ID,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
