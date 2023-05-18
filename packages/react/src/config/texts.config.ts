33adca38c444acb7e4c6b3f6b21fa9118ba6d0db
chavda-bhavik <bhavikvchavda@gmail.com>
2023-05-18 14:39:44 +0530

    feat: Removed Button component from react package

--- packages/react/src/config/texts.config.ts
+++ packages/react/src/config/texts.config.ts
@@ -1,4 +1,5 @@
 export const TEXTS = {
   PROJECTID_NOT_SPECIFIED: 'It looks like you have not specified projectId to the import button which is required!',
   IMPLER_UNDEFINED_ERROR: 'It looks like you have not added embed script or your embed script path is incorrect!',
+  IMPLER_NOT_INITIATED: 'It looks like Impler is not initiated!',
 };
