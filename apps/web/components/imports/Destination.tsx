0a43adcaa8a4c39fbc193baacf6f89ee3db5e0c7
chavda-bhavik <bhavikvchavda@gmail.com>
2023-06-06 17:03:36 +0530

    feat: Grouped sections into Accordian

--- apps/web/components/imports/Destination.tsx
+++ apps/web/components/imports/Destination.tsx
@@ -1,12 +1,11 @@
 import { Prism } from '@mantine/prism';
 import { ITemplate } from '@impler/shared';
-import { Code, Divider, Stack, Text } from '@mantine/core';
+import { Code, Stack, Accordion, Title, useMantineColorScheme } from '@mantine/core';
 
 import { REGULAR_EXPRESSIONS, colors } from '@config';
 import { Input } from '@ui/input';
 import { Button } from '@ui/button';
 import { APIBlock } from '@ui/api-block';
-import { SectionBlock } from '@ui/section-block';
 import { useDestination } from '@hooks/useDestination';
 
 interface DestinationProps {
@@ -14,52 +13,77 @@ interface DestinationProps {
 }
 
 export function Destination({ template }: DestinationProps) {
+  const { colorScheme } = useMantineColorScheme();
   const { register, errors, onSubmit, isUpdateImportLoading } = useDestination({ template });
 
   return (
     <>
-      <Text pb="sm">
-        To get imported data you can provide Webhook URL or You can call our API to get valid and invalid data.
-      </Text>
-      <SectionBlock title="Add Script">
-        <form onSubmit={onSubmit}>
-          <Stack spacing="xs">
-            <Input
-              placeholder="Callback URL"
-              register={register('callbackUrl', {
-                pattern: REGULAR_EXPRESSIONS.URL,
-              })}
-              error={errors.callbackUrl ? 'Please enter valid URL' : undefined}
-            />
-            <Input placeholder="Auth Header Name" register={register('authHeaderName')} />
-            <Button loading={isUpdateImportLoading} type="submit">
-              Save
-            </Button>
-          </Stack>
-        </form>
-      </SectionBlock>
-      <Divider my="sm" variant="dotted" />
-      <Stack spacing="sm">
-        <APIBlock
-          method="GET"
-          title="GET valid data of imported file"
-          url="https://api.impler.io/v1/upload/{uploadId}/rows/valid"
-        />
-        <APIBlock
-          method="GET"
-          title="Get Invalid data of imported file"
-          url="https://api.impler.io/v1/upload/{uploadId}/rows/invalid"
-        />
-        <SectionBlock title="How to get uploadId?">
-          <Text style={{ lineHeight: '1.5rem', color: colors.TXTSecondaryDark }}>
-            You will get uploadId in <Code>onUploadComplete</Code> callback of <Code>@impler/react</Code> package, at
-            the time of import is completed.
-          </Text>
-          <Prism language="tsx">{`import { Import } from '@impler/react';
+      <Accordion variant="contained" radius={0} defaultValue="webhook">
+        <Accordion.Item value="webhook">
+          <Accordion.Control>
+            <Title color={colorScheme === 'dark' ? colors.white : colors.black} order={4}>
+              Webhook
+            </Title>
+            <Title order={5} fw="normal" color={colors.TXTSecondaryDark}>
+              Provide webhook to receive data
+            </Title>
+          </Accordion.Control>
+          <Accordion.Panel>
+            <form onSubmit={onSubmit}>
+              <Stack spacing="xs">
+                <Input
+                  placeholder="Callback URL"
+                  register={register('callbackUrl', {
+                    pattern: REGULAR_EXPRESSIONS.URL,
+                  })}
+                  error={errors.callbackUrl ? 'Please enter valid URL' : undefined}
+                />
+                <Input placeholder="Auth Header Name" register={register('authHeaderName')} />
+                <Button loading={isUpdateImportLoading} type="submit">
+                  Save
+                </Button>
+              </Stack>
+            </form>
+          </Accordion.Panel>
+        </Accordion.Item>
+
+        <Accordion.Item value="api">
+          <Accordion.Control>
+            <Title color={colorScheme === 'dark' ? colors.white : colors.black} order={4}>
+              API
+            </Title>
+            <Title order={5} fw="normal" color={colors.TXTSecondaryDark}>
+              Use API to get data
+            </Title>
+          </Accordion.Control>
+          <Accordion.Panel>
+            <Stack spacing="sm">
+              <APIBlock
+                method="GET"
+                title="GET valid data of imported file"
+                url="https://api.impler.io/v1/upload/{uploadId}/rows/valid"
+              />
+              <APIBlock
+                method="GET"
+                title="Get Invalid data of imported file"
+                url="https://api.impler.io/v1/upload/{uploadId}/rows/invalid"
+              />
+              <Stack spacing="xs">
+                <div>
+                  <Title order={4}>How to get uploadId?</Title>
+                  <Title order={5} fw="normal" color={colorScheme === 'dark' ? colors.TXTGray : colors.TXTLight}>
+                    You will get uploadId in <Code>onUploadComplete</Code> callback of <Code>@impler/react</Code>{' '}
+                    package, at the time of import is completed.
+                  </Title>
+                </div>
+                <Prism language="tsx">{`import { Import } from '@impler/react';
         \n<Button projectId="<PROJECT_ID>" template="<CODE_OR_ID>" onUploadComplete={(upload) => console.log(upload)}>
         Import\n</Button>`}</Prism>
-        </SectionBlock>
-      </Stack>
+              </Stack>
+            </Stack>
+          </Accordion.Panel>
+        </Accordion.Item>
+      </Accordion>
     </>
   );
 }
--- apps/web/components/imports/Editor.tsx
+++ apps/web/components/imports/Editor.tsx
@@ -1,9 +1,9 @@
 import { Controller } from 'react-hook-form';
-import { Group, List, Stack, Text } from '@mantine/core';
+import { Accordion, Group, List, Title, Text, useMantineColorScheme } from '@mantine/core';
 
+import { colors } from '@config';
 import { Button } from '@ui/button';
 import { Editor } from '@ui/editor/Editor';
-import { SectionBlock } from '@ui/section-block';
 import { useEditor } from '@hooks/useEditor';
 
 function PossibleJSONErrors() {
@@ -23,52 +23,78 @@ interface OutputEditorProps {
 }
 
 export default function OutputEditor({ templateId }: OutputEditorProps) {
+  const { colorScheme } = useMantineColorScheme();
   const { customization, control, errors, onSaveClick } = useEditor({ templateId });
 
   return (
-    <Stack spacing="sm">
+    <>
       <Group position="apart">
-        <Text>Customize how you will receive the data</Text>
+        <Text fw="normal" color={colors.TXTDark}>
+          Customize the format of how data will be sent to your destination.
+        </Text>
         <Group>
           <Button onClick={onSaveClick}>Save</Button>
         </Group>
       </Group>
 
-      <SectionBlock title="Customize individual record Item">
-        <Controller
-          control={control}
-          name="recordFormat"
-          render={({ field }) => (
-            <Editor
-              name="recordItem"
-              id="record-item"
-              value={field.value}
-              onChange={field.onChange}
-              variables={customization?.recordVariables}
+      <Accordion mt="sm" variant="contained" radius={0}>
+        <Accordion.Item value="record">
+          <Accordion.Control>
+            <Title color={colorScheme === 'dark' ? colors.white : colors.black} order={4}>
+              Customize individual record
+            </Title>
+            <Title order={5} fw="normal" color={colors.TXTSecondaryDark}>
+              Customize the structure of individual record.
+            </Title>
+          </Accordion.Control>
+          <Accordion.Panel>
+            <Controller
+              control={control}
+              name="recordFormat"
+              render={({ field }) => (
+                <Editor
+                  name="recordItem"
+                  id="record-item"
+                  value={field.value}
+                  onChange={field.onChange}
+                  variables={customization?.recordVariables}
+                />
+              )}
             />
-          )}
-        />
-        {errors.recordFormat?.message && <Text color="red">{errors.recordFormat.message}</Text>}
-        {errors.recordFormat?.type === 'JSON' && <PossibleJSONErrors />}
-      </SectionBlock>
+            {errors.recordFormat?.message && <Text color="red">{errors.recordFormat.message}</Text>}
+            {errors.recordFormat?.type === 'JSON' && <PossibleJSONErrors />}
+          </Accordion.Panel>
+        </Accordion.Item>
 
-      <SectionBlock title="Customize chunk Format">
-        <Controller
-          control={control}
-          name="chunkFormat"
-          render={({ field }) => (
-            <Editor
+        <Accordion.Item value="chunk">
+          <Accordion.Control>
+            <Title color={colorScheme === 'dark' ? colors.white : colors.black} order={4}>
+              Customize chunk Format
+            </Title>
+            <Title order={5} fw="normal" color={colors.TXTSecondaryDark}>
+              Customize the structure of chunk. Imported records are divided into chunks and sent to destination. Chunk
+              includes array of record.
+            </Title>
+          </Accordion.Control>
+          <Accordion.Panel>
+            <Controller
+              control={control}
               name="chunkFormat"
-              id="chunk-item"
-              value={field.value}
-              onChange={field.onChange}
-              variables={customization?.chunkVariables}
+              render={({ field }) => (
+                <Editor
+                  name="chunkFormat"
+                  id="chunk-item"
+                  value={field.value}
+                  onChange={field.onChange}
+                  variables={customization?.chunkVariables}
+                />
+              )}
             />
-          )}
-        />
-        {errors.chunkFormat?.message && <Text color="red">{errors.chunkFormat.message}</Text>}
-        {errors.chunkFormat?.type === 'JSON' && <PossibleJSONErrors />}
-      </SectionBlock>
-    </Stack>
+            {errors.chunkFormat?.message && <Text color="red">{errors.chunkFormat.message}</Text>}
+            {errors.chunkFormat?.type === 'JSON' && <PossibleJSONErrors />}
+          </Accordion.Panel>
+        </Accordion.Item>
+      </Accordion>
+    </>
   );
 }
