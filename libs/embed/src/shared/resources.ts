306e761b555d42f6fede49c6f54d5294072fb298
chavda-bhavik <bhavikvchavda@gmail.com>
2023-09-26 16:35:42 +0530

    feat: Removed SENTRY_DSN from api env

--- libs/embed/src/shared/resources.ts
+++ libs/embed/src/shared/resources.ts
@@ -2,7 +2,6 @@ let ResourcesConstants: {
   API_URL: string;
   IFRAME_URL?: string;
   WWW_URL?: string;
-  SENTRY_DSN: string;
 };
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -12,7 +11,6 @@ if (process.env.ENVIRONMENT && process.env.ENVIRONMENT === '') {
     API_URL: '',
     IFRAME_URL: process.env.WIDGET_URL,
     WWW_URL: process.env.WIDGET_URL,
-    SENTRY_DSN: '',
   };
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
@@ -21,14 +19,12 @@ if (process.env.ENVIRONMENT && process.env.ENVIRONMENT === '') {
     API_URL: '',
     IFRAME_URL: process.env.WIDGET_URL || 'http://localhost:3500',
     WWW_URL: process.env.WIDGET_URL || 'http://localhost:3500',
-    SENTRY_DSN: '',
   };
 } else {
   ResourcesConstants = {
     API_URL: '',
     IFRAME_URL: process.env.WIDGET_URL || 'http://localhost:3500',
     WWW_URL: process.env.WIDGET_URL || 'http://localhost:3500',
-    SENTRY_DSN: '',
   };
 }
 
@@ -36,4 +32,3 @@ export const DEBUG = false;
 export const { API_URL } = ResourcesConstants;
 export const { IFRAME_URL } = ResourcesConstants;
 export const { WWW_URL } = ResourcesConstants;
-export const { SENTRY_DSN } = ResourcesConstants;
