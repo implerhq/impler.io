9b4a2688960b09a917a200a98642cea77b09503c
chavda-bhavik <bhavikvchavda@gmail.com>
2024-01-08 15:19:11 +0530

    fix: Widget pagination conflicts with continuous deletes

--- apps/widget/src/hooks/Phase3/usePhase3.tsx
+++ apps/widget/src/hooks/Phase3/usePhase3.tsx
@@ -128,6 +128,12 @@ export function usePhase3({ onNext }: IUsePhase3Props) {
       api.getReviewData({ uploadId: uploadInfo._id, page: reviewPageNumber, type: reviewDataType }),
     {
       onSuccess(data) {
+        if (!data.data.length && page > 1) {
+          setTotalPages(totalPages - 1);
+          setPage(page - 1);
+
+          return;
+        }
         setReviewData(data.data);
         logAmplitudeEvent('VALIDATE', {
           invalidRecords: data.totalRecords,
@@ -150,7 +156,7 @@ export function usePhase3({ onNext }: IUsePhase3Props) {
     staleTime: 0,
     onSuccess() {
       fetchUploadInfo();
-      refetchReviewData([defaultPage, type]);
+      refetchReviewData([page, type]);
     },
     onError(error: IErrorObject) {
       notifier.showError({ message: error.message, title: error.error });
@@ -183,25 +189,28 @@ export function usePhase3({ onNext }: IUsePhase3Props) {
   const { mutate: updateRecord } = useMutation<unknown, IErrorObject, IRecord, [string]>([`update`], (record) =>
     api.updateRecord(uploadInfo._id, record)
   );
-  const { mutate: deleteRecord } = useMutation<unknown, IErrorObject, [number, boolean], [string]>(
-    [`delete`],
-    ([index, isValid]) => api.deleteRecord(uploadInfo._id, index, isValid),
-    {
-      onSuccess(data, vars) {
-        const newReviewData = reviewData.filter((record) => record.index !== vars[0]);
-        const newUploadInfo = { ...uploadInfo };
-        newUploadInfo.totalRecords = newUploadInfo.totalRecords - 1;
-        if (!vars[1]) {
-          newUploadInfo.invalidRecords = newUploadInfo.invalidRecords - 1;
-        }
-        setUploadInfo(newUploadInfo);
-        setReviewData(newReviewData);
-        if (newReviewData.length === 0) {
-          refetchReviewData([defaultPage, type]);
-        }
-      },
-    }
-  );
+  const { mutate: deleteRecord, isLoading: isDeleteRecordLoading } = useMutation<
+    unknown,
+    IErrorObject,
+    [number, boolean],
+    [string]
+  >([`delete`], ([index, isValid]) => api.deleteRecord(uploadInfo._id, index, isValid), {
+    onSuccess(data, vars) {
+      const newReviewData = reviewData.filter((record) => record.index !== vars[0]);
+      const newUploadInfo = { ...uploadInfo };
+      newUploadInfo.totalRecords = newUploadInfo.totalRecords - 1;
+      if (!vars[1]) {
+        newUploadInfo.invalidRecords = newUploadInfo.invalidRecords - 1;
+      }
+      setUploadInfo(newUploadInfo);
+      setReviewData(newReviewData);
+      if (newReviewData.length === 0) {
+        refetchReviewData([getPrevPage(page), type]);
+      }
+    },
+  });
+
+  const getPrevPage = (currentPageNumber: number) => Math.max(currentPageNumber - 1, 1);
 
   const onTypeChange = (newType: ReviewDataTypesEnum) => {
     setType(newType);
@@ -226,6 +235,7 @@ export function usePhase3({ onNext }: IUsePhase3Props) {
     isDoReviewLoading,
     isReviewDataLoading,
     showAllDataValidModal,
+    isDeleteRecordLoading,
     isConfirmReviewLoading,
     setShowAllDataValidModal,
     reviewData: reviewData || [],
