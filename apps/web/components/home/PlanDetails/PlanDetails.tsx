9a759b7ea985b70c4e20d396fc11517776e71420
Mayur <mayurkaikadi@knovator.in>
2024-04-26 10:45:00 +0530

    feat: Modal added in PlanDetails

--- apps/web/components/home/PlanDetails/PlanDetails.tsx
+++ apps/web/components/home/PlanDetails/PlanDetails.tsx
@@ -1,7 +1,17 @@
 import { colors } from '@config';
-import { Title, Text, Stack, Flex, Button } from '@mantine/core';
+import { Title, Text, Stack, Flex, Button, Skeleton } from '@mantine/core';
+import { PlansModal } from '@components/plans-modal/PlansModal';
+import { modals } from '@mantine/modals';
+import { useApp } from '@hooks/useApp';
+import { usePlanDetails } from '@hooks/usePlanDetails';
 
 export function PlanDetails() {
+  const { profile, isProfileLoading } = useApp();
+
+  const data = usePlanDetails(profile);
+
+  if (isProfileLoading) return <Skeleton width="100%" height="200" />;
+
   return (
     <Flex
       p="sm"
@@ -13,10 +23,21 @@ export function PlanDetails() {
       <Stack spacing="xs" style={{ flexGrow: 1 }}>
         <Title order={4}>Usage</Title>
         <Text>
-          You have imported {10} of {1000} records this month on the {'Sandbox'} plan (resets on {'24-05-2024'})
+          You have imported {data?.usage.IMPORTED_ROWS} of {1000} records this month on the {data?.plan.name} plan
+          resets on {data?.expiryDate}
         </Text>
       </Stack>
-      <Button color="yellow">Upgrade Plan</Button>
+      <Button
+        onClick={() => {
+          modals.open({
+            children: <PlansModal modalTitle={'Choose Your Plan'} userProfile={profile!} />,
+            size: 'xl',
+          });
+        }}
+        color="yellow"
+      >
+        Upgrade Plan
+      </Button>
     </Flex>
   );
 }
