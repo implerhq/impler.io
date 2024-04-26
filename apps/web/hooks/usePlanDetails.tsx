79b2f01ae307681ca5097f68307de2110cbfc16e
Mayur <mayurkaikadi@knovator.in>
2024-04-26 11:50:15 +0530

    feat: Fetch dynamic header key and valuefrom next config and pass it in react query

--- apps/web/hooks/usePlanDetails.tsx
+++ apps/web/hooks/usePlanDetails.tsx
@@ -2,19 +2,24 @@ import { useQuery } from '@tanstack/react-query';
 import { commonApi } from '@libs/api';
 import { API_KEYS } from '@config';
 import { IErrorObject } from '@impler/shared';
+import getConfig from 'next/config';
 
 interface UsePlanDetailProps {
   email: string;
 }
 
+const { publicRuntimeConfig } = getConfig();
+const authKey = publicRuntimeConfig.NEXT_PUBLIC_PAYMENT_GATEWAY_AUTH_HEADER_KEY;
+const authValue = publicRuntimeConfig.NEXT_PUBLIC_PAYMENT_GATEWAY_AUTH_HEADER_VALUE;
+
 export function usePlanDetails({ email }: UsePlanDetailProps) {
-  const { data } = useQuery<unknown, IErrorObject, ISubscriptionData, [string]>(
-    [API_KEYS.FETCH_ACTIVE_SUBSCRIPTION],
+  const { data } = useQuery<unknown, IErrorObject, ISubscriptionData, [string, string]>(
+    [API_KEYS.FETCH_ACTIVE_SUBSCRIPTION, email],
     () =>
-      commonApi<unknown>(API_KEYS.FETCH_ACTIVE_SUBSCRIPTION as any, {
+      commonApi<ISubscriptionData>(API_KEYS.FETCH_ACTIVE_SUBSCRIPTION as any, {
         baseUrl: process.env.NEXT_PUBLIC_PAYMENT_GATEWAY_URL,
         headers: {
-          auth: 'auth',
+          [authKey]: authValue,
         },
         parameters: [email],
       })
