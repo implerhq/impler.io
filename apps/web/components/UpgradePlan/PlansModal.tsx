157d6f954b46a496a730f74782d15422918291d8
Mayur <mayurkaikadi@knovator.in>
2024-05-09 17:51:39 +0530

    feat: Passing Expiry date and CanceledOn props to Plans component

--- apps/web/components/UpgradePlan/PlansModal.tsx
+++ apps/web/components/UpgradePlan/PlansModal.tsx
@@ -5,9 +5,11 @@ import { Plans } from './Plans';
 interface PlanProps {
   userProfile: IProfileData;
   activePlanCode?: string;
+  canceledOn?: Date;
+  expiryDate: Date;
 }
 
-export const PlansModal = ({ userProfile, activePlanCode }: PlanProps) => {
+export const PlansModal = ({ userProfile, activePlanCode, canceledOn, expiryDate }: PlanProps) => {
   return (
     <Container fluid={true}>
       <Stack align="center">
@@ -15,7 +17,7 @@ export const PlansModal = ({ userProfile, activePlanCode }: PlanProps) => {
           Choose the plan that works best for you
         </Title>
 
-        <Plans profile={userProfile} activePlanCode={activePlanCode} />
+        <Plans profile={userProfile} activePlanCode={activePlanCode} canceledOn={canceledOn} expiryDate={expiryDate} />
       </Stack>
     </Container>
   );
