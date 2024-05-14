0532f35ec99fefa63aacab38d168a03da63b4235
Mayur <mayurkaikadi@knovator.in>
2024-05-14 19:22:54 +0530

    feat: UpdateUser introduced in the USE_CASES

--- apps/api/src/app/user/usecases/index.ts
+++ apps/api/src/app/user/usecases/index.ts
@@ -1,12 +1,14 @@
 import { GetImportCounts } from './get-import-count/get-import-count.usecase';
 import { GetActiveSubscription } from './get-active-subscription/get-active-subscription.usecase';
 import { CancelSubscription } from './cancel-subscription/cancel-subscription.usecase';
+import { UpdateUser } from './update-user/update-user.usecase';
 
 export const USE_CASES = [
   GetImportCounts,
   CancelSubscription,
   GetActiveSubscription,
+  UpdateUser,
   //
 ];
 
-export { GetImportCounts, CancelSubscription, GetActiveSubscription };
+export { GetImportCounts, CancelSubscription, GetActiveSubscription, UpdateUser };
