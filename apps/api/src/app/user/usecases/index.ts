e579472352e5e323b91c991386a2cf7392e86ce1
Mayur <mayurkaikadi@knovator.in>
2024-05-15 11:18:09 +0530

    feat: Added SetupPaymentIntent usecase

--- apps/api/src/app/user/usecases/index.ts
+++ apps/api/src/app/user/usecases/index.ts
@@ -1,14 +1,14 @@
 import { GetImportCounts } from './get-import-count/get-import-count.usecase';
 import { GetActiveSubscription } from './get-active-subscription/get-active-subscription.usecase';
 import { CancelSubscription } from './cancel-subscription/cancel-subscription.usecase';
-import { UpdateUser } from './update-user/update-user.usecase';
+import { SetupPaymentIntent } from './setup-payment-intent/setup-payment-intent.usecase';
 
 export const USE_CASES = [
   GetImportCounts,
   CancelSubscription,
   GetActiveSubscription,
-  UpdateUser,
+  SetupPaymentIntent,
   //
 ];
 
-export { GetImportCounts, CancelSubscription, GetActiveSubscription, UpdateUser };
+export { GetImportCounts, CancelSubscription, GetActiveSubscription, SetupPaymentIntent };
