4a5acc8f0a18b8743b3e0305580cc4fc35d7a774
Mayur <mayurkaikadi@knovator.in>
2024-05-16 11:18:02 +0530

    feat: Added new usecase SavePaymentIntentId

--- apps/api/src/app/user/usecases/index.ts
+++ apps/api/src/app/user/usecases/index.ts
@@ -2,13 +2,15 @@ import { GetImportCounts } from './get-import-count/get-import-count.usecase';
 import { GetActiveSubscription } from './get-active-subscription/get-active-subscription.usecase';
 import { CancelSubscription } from './cancel-subscription/cancel-subscription.usecase';
 import { SetupPaymentIntent } from './setup-payment-intent/setup-payment-intent.usecase';
+import { SavePaymentIntentId } from './save-payment-intent-id/save-paymentintentid.usecase';
 
 export const USE_CASES = [
   GetImportCounts,
   CancelSubscription,
   GetActiveSubscription,
   SetupPaymentIntent,
+  SavePaymentIntentId,
   //
 ];
 
-export { GetImportCounts, CancelSubscription, GetActiveSubscription, SetupPaymentIntent };
+export { GetImportCounts, CancelSubscription, GetActiveSubscription, SetupPaymentIntent, SavePaymentIntentId };
