4df54e5078d737f3f2ca9ab34784eba62143e22d
Mayur <mayurkaikadi@knovator.in>
2024-05-21 11:10:12 +0530

    feat: Added Card component to show the cards added by the user

---
+++ apps/web/components/settings/Card.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Image from 'next/image';
+import { ICardData } from '@impler/shared';
+import { Flex, Group, Stack, Text } from '@mantine/core';
+
+import { colors } from '@config';
+import { IconButton } from '@ui/icon-button';
+import { DeleteIcon } from '@assets/icons/Delete.icon';
+
+interface CardProps {
+  data: ICardData;
+  onRemoveCardClick: () => void;
+}
+
+export function Card({ data, onRemoveCardClick }: CardProps) {
+  const src = data.brand.toLowerCase()?.replaceAll(' ', '_') || 'default';
+  const expiry = `${data?.expMonth}/${data?.expYear}`;
+
+  return (
+    <Stack bg="gray" p="xs" spacing="xs" style={{ flexGrow: 1 }}>
+      <Flex justify="space-between">
+        <Text fs="italic" size="md">
+          xxxx {data.last4Digits}
+        </Text>
+        <IconButton onClick={onRemoveCardClick} label="Remove Card">
+          <DeleteIcon color={colors.danger} />
+        </IconButton>
+      </Flex>
+      <Flex justify="space-between" gap="xs" wrap="wrap">
+        <Group spacing="xs">
+          <Text fw="lighter">Expiry:</Text>
+          <Text tt="uppercase" size="md" fw="bold" className="uppercase text-xl font-bold">
+            {expiry}
+          </Text>
+        </Group>
+        <Image width={75} height={35} src={`/images/cards/${src}.png`} alt="CC" />
+      </Flex>
+    </Stack>
+  );
+}
