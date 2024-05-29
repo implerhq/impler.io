6620b691daf69cd8f4ce1579b7823527f433112a
Mayur <mayurkaikadi@knovator.in>
2024-05-29 19:41:01 +0530

    feat: Fetched parameters from url to show the card add modal

--- apps/web/components/settings/UserCards.tsx
+++ apps/web/components/settings/UserCards.tsx
@@ -4,9 +4,25 @@ import { Button } from '@ui/button';
 import { AddCardModal, Card } from '@components/settings';
 import { usePaymentMethods } from '@hooks/usePaymentMethods';
 import { modals } from '@mantine/modals';
+import getConfig from 'next/config';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export function UserCards() {
+  const router = useRouter();
+  const { tab, action } = router.query;
   const [opened, { open, close }] = useDisclosure(false);
+
+  useEffect(() => {
+    if (tab === 'addcard' && action === 'addcardmodal') {
+      open();
+    }
+  }, []);
+
+  const { publicRuntimeConfig } = getConfig();
+  const defaultPaymentGateway = publicRuntimeConfig.NEXT_PUBLIC_DEFAULT_PAYMENT_GATEWAY;
+  const razorpayKey = publicRuntimeConfig.NEXT_PUBLIC_RAZORPAY_KEY;
+
   const {
     refetchPaymentMethods,
     paymentMethods,
@@ -15,12 +31,42 @@ export function UserCards() {
     isPaymentMethodsLoading,
   } = usePaymentMethods();
 
+  const handleAuthorizationTransaction = async () => {
+    if (defaultPaymentGateway === 'RAZORPAY') {
+      const options = {
+        key: razorpayKey,
+        order_id: 'order_OFwYpDKdDnAvJr',
+        customer_id: 'cust_ODrMF3Khtptkl1',
+        recurring: '1',
+        handler: function (response: any) {
+          alert('Payment Successful!\nPayment ID: ' + response.razorpay_payment_id);
+          alert('Order ID: ' + response.razorpay_order_id);
+          alert('Signature: ' + response.razorpay_signature);
+        },
+        notes: {
+          note_key1: 'value1',
+          note_key2: 'value2',
+        },
+        theme: {
+          color: '#F37254',
+        },
+      };
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      const rzp1 = new Razorpay(options);
+
+      rzp1.open();
+    } else {
+      open();
+    }
+    // Logic to handle the specific URL
+  };
+
   function handleDeleteCard(card: any) {
     modals.openConfirmModal({
       title: 'Please confirm your action',
       children: 'Are you sure you want to delete your card?',
       labels: { confirm: 'Confirm', cancel: 'Cancel' },
-      onCancel: () => console.log('Cancel'),
       onConfirm: async () => {
         deletePaymentMethod(card.paymentMethodId);
         refetchPaymentMethods();
@@ -42,7 +88,7 @@ export function UserCards() {
             />
           ))}
         </SimpleGrid>
-        <Button onClick={open}>Add Card</Button>
+        <Button onClick={handleAuthorizationTransaction}>Add Card</Button>
       </Stack>
 
       <Modal opened={opened} withCloseButton onClose={close} title="Add Your Card Details" centered>
