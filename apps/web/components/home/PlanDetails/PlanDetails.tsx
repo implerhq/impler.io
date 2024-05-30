1da3350d1390922a6d1074470ca533b90b6fd1b3
chavda-bhavik <bhavikvchavda@gmail.com>
2024-05-30 15:38:51 +0530

    fix: Build issue

--- apps/web/components/home/PlanDetails/PlanDetails.tsx
+++ apps/web/components/home/PlanDetails/PlanDetails.tsx
@@ -12,9 +12,9 @@ export function PlanDetails() {
     email: profile?.email ?? '',
   });
 
-  if (!activePlanDetails) return;
-
   if (isActivePlanLoading) return <Skeleton width="100%" height="200" />;
+
+  if (!activePlanDetails) return null;
 
   let numberOfRecords;
   if (typeof activePlanDetails?.meta.IMPORTED_ROWS === 'number') {
@@ -34,8 +34,6 @@ export function PlanDetails() {
   // Define background color based on conditions
   const backgroundColor =
     isLessThanZero || activePlanDetails!.usage.IMPORTED_ROWS > numberOfRecords ? colors.danger : colors.yellow;
-
-  if (!activePlanDetails) return null;
 
   return (
     <>
