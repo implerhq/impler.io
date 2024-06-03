f77df4c5b57310435f44b44b692c238b56770beb
chavda-bhavik <bhavikvchavda@gmail.com>
2024-06-03 15:36:15 +0530

    feat: Sent value in number when type is number

--- apps/api/src/app/review/usecases/do-review/do-review.usecase.ts
+++ apps/api/src/app/review/usecases/do-review/do-review.usecase.ts
@@ -3,7 +3,7 @@ import { Writable } from 'stream';
 import { Injectable, BadRequestException } from '@nestjs/common';
 
 import { APIMessages } from '@shared/constants';
-import { ITemplateSchemaItem, UploadStatusEnum } from '@impler/shared';
+import { ColumnTypesEnum, ITemplateSchemaItem, UploadStatusEnum } from '@impler/shared';
 import { BaseReview } from './base-review.usecase';
 import { BATCH_LIMIT } from '@shared/services/sandbox';
 import { StorageService } from '@impler/shared/dist/services/storage';
@@ -43,9 +43,11 @@ export class DoReview extends BaseReview {
     const dateFormats: Record<string, string[]> = {};
     const uniqueItems: Record<string, Set<any>> = {};
     const columns = JSON.parse(uploadInfo.customSchema);
-    const multiSelectColumnHeadings = [];
+    const multiSelectColumnHeadings = new Set<string>();
+    const numberColumnHeadings = new Set<string>();
     (columns as ITemplateSchemaItem[]).forEach((column) => {
-      if (column.allowMultiSelect) multiSelectColumnHeadings.push(column.key);
+      if (column.allowMultiSelect) multiSelectColumnHeadings.add(column.key);
+      if (column.type === ColumnTypesEnum.NUMBER) numberColumnHeadings.add(column.key);
     });
     const schema = this.buildAJVSchema({
       columns,
@@ -77,6 +79,7 @@ export class DoReview extends BaseReview {
         extra: uploadInfo.extra,
         dataStream, // not-used
         dateFormats,
+        numberColumnHeadings,
         multiSelectColumnHeadings,
       });
 
@@ -110,6 +113,7 @@ export class DoReview extends BaseReview {
         uploadId: _uploadId,
         validator,
         dateFormats,
+        numberColumnHeadings,
         multiSelectColumnHeadings,
       });
     }
