14cdbbba255666c10353045eee6b8fa3358db8b5
Mayur <mayurkaikadi@knovator.in>
2024-06-12 15:02:02 +0530

    feat: Date has been formatted into desired format

--- apps/api/src/app/user/usecases/get-transaction-history/get-transaction-history.usecase.ts
+++ apps/api/src/app/user/usecases/get-transaction-history/get-transaction-history.usecase.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PaymentAPIService } from '@impler/shared';
+import * as dayjs from 'dayjs';
 
 @Injectable()
 export class GetTransactionHistory {
@@ -9,15 +10,15 @@ export class GetTransactionHistory {
     const transactions = await this.paymentApiService.getTransactionHistory(email);
 
     return transactions.map((transactionItem) => ({
-      transactionDate: transactionItem.transactionDate,
+      transactionDate: dayjs(transactionItem.transactionDate).format('DD-MM-YYYY'),
       planName: transactionItem.planName,
       transactionStatus: transactionItem.transactionStatus,
-      membershipDate: transactionItem.membershipDate,
-      expiryDate: transactionItem.expiryDate,
+      membershipDate: dayjs(transactionItem.membershipDate).format('DD-MM-YYYY'),
+      expiryDate: dayjs(transactionItem.expiryDate).format('DD-MM-YYYY'),
       isPlanActive: transactionItem.isPlanActive,
       charge: transactionItem.charge,
       amount: transactionItem.amount,
-      currency: transactionItem.currency,
+      currency: transactionItem.currency ? transactionItem.currency : 'usd',
       _id: transactionItem.id,
     }));
   }
