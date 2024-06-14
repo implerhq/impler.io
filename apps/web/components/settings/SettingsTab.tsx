ff3f427d944315914fab7cfc12bab6fadbce2726
Mayur <mayurkaikadi@knovator.in>
2024-06-14 18:42:40 +0530

    feat: Settings tab Optimized

--- apps/web/components/settings/SettingsTab.tsx
+++ apps/web/components/settings/SettingsTab.tsx
@@ -2,25 +2,13 @@ import { GenerateAccessToken } from './GenerateAccessToken';
 import { UserCards } from './UserCards';
 
 import { Tabs } from '@mantine/core';
-import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 export function SettingsTab() {
   const router = useRouter();
-  const { tab } = router.query;
-
-  const handleTabChange = (value: string) => {
-    router.push(`/settings?tab=${value}`);
-  };
-
-  useEffect(() => {
-    if (!tab) {
-      router.replace(`/settings?tab=accesstoken`);
-    }
-  });
 
   return (
-    <Tabs mt={2} defaultValue="accesstoken" value={(tab as string) || 'accesstoken'} onTabChange={handleTabChange}>
+    <Tabs mt={2} defaultValue={(router.query.tab as string) || 'accesstoken'}>
       <Tabs.List>
         <Tabs.Tab value="accesstoken">Access Token</Tabs.Tab>
         <Tabs.Tab value="addcard">Cards</Tabs.Tab>
