70cedc76c4dbc168360b90c1d51dc7de66dc2b9f
Mayur <mayurkaikadi@knovator.in>
2024-06-14 13:53:01 +0530

    feat: Cards Added

--- apps/web/components/settings/UserCards.tsx
+++ apps/web/components/settings/UserCards.tsx
@@ -1,28 +1,19 @@
-import { useDisclosure } from '@mantine/hooks';
-import { Modal, SimpleGrid, Stack, Box, LoadingOverlay } from '@mantine/core';
-import { Button } from '@ui/button';
-import { AddCardModal, Card } from '@components/settings';
-import { usePaymentMethods } from '@hooks/usePaymentMethods';
-import { modals } from '@mantine/modals';
-import getConfig from 'next/config';
+import { Stack } from '@mantine/core';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { modals } from '@mantine/modals';
+import React, { useEffect } from 'react';
+import { useDisclosure } from '@mantine/hooks';
+import { Modal, SimpleGrid, Box, LoadingOverlay } from '@mantine/core';
+
+import { Card } from './Card';
+import { AddCard } from './AddCard';
+import { AddCardModal } from '@components/settings';
+import { usePaymentMethods } from '@hooks/usePaymentMethods';
 
 export function UserCards() {
   const router = useRouter();
   const { tab, action } = router.query;
   const [opened, { open, close }] = useDisclosure(false);
-
-  useEffect(() => {
-    if (tab === 'addcard' && action === 'addcardmodal') {
-      open();
-    }
-  }, []);
-
-  const { publicRuntimeConfig } = getConfig();
-  const defaultPaymentGateway = publicRuntimeConfig.NEXT_PUBLIC_DEFAULT_PAYMENT_GATEWAY;
-  const razorpayKey = publicRuntimeConfig.NEXT_PUBLIC_RAZORPAY_KEY;
-
   const {
     refetchPaymentMethods,
     paymentMethods,
@@ -31,68 +22,43 @@ export function UserCards() {
     isPaymentMethodsLoading,
   } = usePaymentMethods();
 
-  const handleAuthorizationTransaction = async () => {
-    if (defaultPaymentGateway === 'RAZORPAY') {
-      const options = {
-        key: razorpayKey,
-        order_id: 'order_OFwYpDKdDnAvJr',
-        customer_id: 'cust_ODrMF3Khtptkl1',
-        recurring: '1',
-        handler: function (response: any) {
-          alert('Payment Successful!\nPayment ID: ' + response.razorpay_payment_id);
-          alert('Order ID: ' + response.razorpay_order_id);
-          alert('Signature: ' + response.razorpay_signature);
-        },
-        notes: {
-          note_key1: 'value1',
-          note_key2: 'value2',
-        },
-        theme: {
-          color: '#F37254',
-        },
-      };
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      const rzp1 = new Razorpay(options);
-
-      rzp1.open();
-    } else {
-      open();
-    }
-    // Logic to handle the specific URL
-  };
-
-  function handleDeleteCard(card: any) {
+  function handleDeleteCard(paymentMethodId: string) {
     modals.openConfirmModal({
       title: 'Please confirm your action',
       children: 'Are you sure you want to delete your card?',
       centered: true,
+      confirmProps: { color: 'red' },
       labels: { confirm: 'Confirm', cancel: 'Cancel' },
-      onConfirm: async () => {
-        deletePaymentMethod(card.paymentMethodId);
-        refetchPaymentMethods();
-      },
+      onConfirm: async () => deletePaymentMethod(paymentMethodId),
     });
   }
+
+  useEffect(() => {
+    if (tab === 'addcard' && action === 'addcardmodal') {
+      open();
+    }
+  }, [action, open, tab]);
 
   return (
     <Box pos="relative" mt={20}>
       <LoadingOverlay visible={isDeletePaymentMethodLoading || isPaymentMethodsLoading} />
       <Stack spacing="xs" mt={10}>
-        <SimpleGrid cols={4} spacing="xs">
+        <SimpleGrid
+          spacing="sm"
+          cols={4}
+          breakpoints={[
+            { maxWidth: 'lg', cols: 3, spacing: 'md' },
+            { maxWidth: 'sm', cols: 2, spacing: 'sm' },
+          ]}
+        >
           {paymentMethods?.map((card) => (
-            <Card
-              data={card}
-              key={card.paymentMethodId}
-              refetchPaymentMethods={refetchPaymentMethods}
-              onRemoveCardClick={() => handleDeleteCard(card)}
-            />
+            <Card key={card.paymentMethodId} data={card} onRemoveCardClick={handleDeleteCard} />
           ))}
+          <AddCard onClick={open} />
         </SimpleGrid>
-        <Button onClick={handleAuthorizationTransaction}>Add Card</Button>
       </Stack>
 
-      <Modal opened={opened} withCloseButton onClose={close} title="Add Your Card Details" centered>
+      <Modal opened={opened} withCloseButton onClose={close} title="Add Your Card" centered>
         <AddCardModal refetchPaymentMethods={refetchPaymentMethods} close={close} />
       </Modal>
     </Box>
