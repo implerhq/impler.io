1b5a29c5821bc97841797884c1bfca0825d24b3a
Mayur <mayurkaikadi@knovator.in>
2024-07-08 17:17:19 +0530

    feat: Fetched the template record based on the projectId

--- apps/api/src/app/common/usecases/get-import-config/get-import-config.usecase.ts
+++ apps/api/src/app/common/usecases/get-import-config/get-import-config.usecase.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { UserRepository } from '@impler/dal';
+import { UserRepository, TemplateRepository } from '@impler/dal';
 import { IImportConfig, PaymentAPIService } from '@impler/shared';
 
 @Injectable()
 export class GetImportConfig {
   constructor(
     private userRepository: UserRepository,
-    private paymentAPIService: PaymentAPIService
+    private paymentAPIService: PaymentAPIService,
+    private templateRepository: TemplateRepository
   ) {}
 
   async execute(projectId: string): Promise<IImportConfig> {
@@ -14,6 +15,16 @@ export class GetImportConfig {
 
     const removeBrandingAvailable = await this.paymentAPIService.checkEvent(userEmail, 'REMOVE_BRANDING');
 
-    return { showBranding: !removeBrandingAvailable };
+    const template = await this.templateRepository.findOne({
+      _projectId: projectId,
+    });
+
+    if (!template) {
+      throw new Error('Template not found');
+    }
+
+    const importMode = template.mode;
+
+    return { showBranding: !removeBrandingAvailable, mode: importMode };
   }
 }
