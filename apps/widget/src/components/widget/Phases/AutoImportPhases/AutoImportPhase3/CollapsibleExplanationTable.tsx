f5f3a7284c11cd963496059206af0cb7560aa684
Mayur <mayurkaikadi@knovator.in>
2024-07-11 15:44:37 +0530

    feat: Refactor CollapsibleExplanationTable to destructure props from interface

--- apps/widget/src/components/widget/Phases/AutoImportPhases/AutoImportPhase3/CollapsibleExplanationTable.tsx
+++ apps/widget/src/components/widget/Phases/AutoImportPhases/AutoImportPhase3/CollapsibleExplanationTable.tsx
@@ -7,7 +7,7 @@ interface CollapsibleExplanationTableProps {
   cronExamples: { expression: string; schedule: string }[];
 }
 
-export function CollapsibleExplanationTable(props: CollapsibleExplanationTableProps) {
+export function CollapsibleExplanationTable({ cronExamples, opened, toggle }: CollapsibleExplanationTableProps) {
   return (
     <Paper style={{ marginTop: '32px', padding: '16px', border: '1px solid #f0f0f0', backgroundColor: '#ffffff' }}>
       <Flex
@@ -18,20 +18,20 @@ export function CollapsibleExplanationTable(props: CollapsibleExplanationTablePr
           fontWeight: 'bold',
           fontSize: '18px',
         }}
-        onClick={props.toggle}
+        onClick={toggle}
       >
         <Text mr="sm">Explanation</Text>
         <ChevronDown
           styles={{
             transition: 'transform 0.3s ease',
-            transform: props.opened ? 'rotate(180deg)' : 'rotate(0deg)',
+            transform: opened ? 'rotate(180deg)' : 'rotate(0deg)',
             fontWeight: 'bold',
             fontSize: '18px',
           }}
         />
       </Flex>
 
-      <Collapse in={props.opened}>
+      <Collapse in={opened}>
         <Table highlightOnHover mt="md" style={{ borderCollapse: 'collapse', textAlign: 'center' }}>
           <thead>
             <tr style={{ backgroundColor: '#edf3ff' }}>
@@ -40,7 +40,7 @@ export function CollapsibleExplanationTable(props: CollapsibleExplanationTablePr
             </tr>
           </thead>
           <tbody>
-            {props.cronExamples.map((example, index) => (
+            {cronExamples.map((example, index) => (
               <tr key={index}>
                 <td style={{ padding: '8px', textAlign: 'center' }}>{example.expression}</td>
                 <td style={{ padding: '8px', textAlign: 'center' }}>{example.schedule}</td>
