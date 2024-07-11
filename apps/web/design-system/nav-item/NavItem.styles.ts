98d15e4189618f3124c1d2f71a0a76896b6016c4
chavda-bhavik <bhavikvchavda@gmail.com>
2024-07-11 12:49:38 +0530

    feat: Added documentation to sidebar

--- apps/web/design-system/nav-item/NavItem.styles.ts
+++ apps/web/design-system/nav-item/NavItem.styles.ts
@@ -1,14 +1,14 @@
 import { colors } from '@config';
-import { createStyles, MantineTheme } from '@mantine/core';
+import { createStyles, CSSObject, MantineTheme } from '@mantine/core';
 
-const getRootFilledStyles = (theme: MantineTheme, isActive?: boolean) => ({
+const getRootFilledStyles = (theme: MantineTheme, isActive?: boolean): CSSObject => ({
   display: 'flex',
   flexDirection: 'row',
   justifyContent: 'flex-start',
   alignItems: 'center',
   gap: theme.spacing.xs,
   color: isActive ? colors.blue : theme.colorScheme === 'dark' ? colors.TXTGray : colors.TXTLight,
-  paddingInline: theme.spacing.sm,
+  paddingLeft: theme.spacing.sm,
   flexWrap: 'nowrap',
   transition: 'all 0.2s ease',
   cursor: 'pointer',
@@ -17,30 +17,6 @@ const getRootFilledStyles = (theme: MantineTheme, isActive?: boolean) => ({
   '&:hover': {
     color: colors.blue,
   },
-  '&:hover:before': {
-    content: '""',
-    display: 'block',
-    width: '3px',
-    height: '100%',
-    backgroundColor: colors.blue,
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    borderRadius: '0 3px 3px 0',
-  },
-  ...(isActive && {
-    '&:before': {
-      content: '""',
-      display: 'block',
-      width: '3px',
-      height: '100%',
-      backgroundColor: colors.blue,
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      borderRadius: '0 3px 3px 0',
-    },
-  }),
 });
 
 const getLinkStyles = () => ({
--- apps/web/design-system/nav-item/NavItem.tsx
+++ apps/web/design-system/nav-item/NavItem.tsx
@@ -1,19 +1,21 @@
-import useStyles from './NavItem.styles';
-import { Flex, Text } from '@mantine/core';
 import Link from 'next/link';
+import { Flex, Text } from '@mantine/core';
+import { HTMLAttributeAnchorTarget } from 'react';
+import useStyles from './NavItem.styles';
 
 interface NavItemProps {
   icon: any;
   title: string;
   active?: boolean;
   href: string;
+  target?: HTMLAttributeAnchorTarget;
 }
 
-export function NavItem({ icon, title, active, href }: NavItemProps) {
+export function NavItem({ target, icon, title, active, href }: NavItemProps) {
   const { classes } = useStyles({ active });
 
   return (
-    <Link href={href} className={classes.link}>
+    <Link href={href} target={target} className={classes.link}>
       <Flex className={classes.root}>
         {icon}
         <Text size="lg">{title}</Text>
