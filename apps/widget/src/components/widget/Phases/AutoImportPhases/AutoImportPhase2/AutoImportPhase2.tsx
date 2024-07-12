486c55e793b0a250a145baf0d734a5be4387a05b
Mayur <mayurkaikadi@knovator.in>
2024-07-12 16:59:12 +0530

    feat: Basic UI for AutoImportPhase2 implemented

--- apps/widget/src/components/widget/Phases/AutoImportPhases/AutoImportPhase2/AutoImportPhase2.tsx
+++ apps/widget/src/components/widget/Phases/AutoImportPhases/AutoImportPhase2/AutoImportPhase2.tsx
@@ -1,15 +1,50 @@
-import { Text } from '@mantine/core';
+import { useEffect, useRef, useState } from 'react';
+import { Group, Stack } from '@mantine/core';
 import { PhasesEnum } from '@types';
+import useStyles from './Styles';
 import { AutoImportFooter } from 'components/Common/Footer/AutoImportFooter';
+import { MappingHeading } from './MappingHeading';
+import { MappingItem } from '@ui/MappingItem';
 
 interface IAutoImportPhase2Props {
   onNextClick: () => void;
 }
 
 export function AutoImportPhase2({ onNextClick }: IAutoImportPhase2Props) {
+  const defaulWrappertHeight = 200;
+  const { classes } = useStyles();
+  const [wrapperHeight, setWrapperHeight] = useState(defaulWrappertHeight);
+
+  const wrapperRef = useRef<HTMLDivElement>() as React.MutableRefObject<HTMLDivElement>;
+  const titlesRef = useRef<HTMLDivElement>() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    // setting wrapper height
+    setWrapperHeight(
+      wrapperRef.current.getBoundingClientRect().height - titlesRef.current.getBoundingClientRect().height
+    );
+  }, []);
+
   return (
     <>
-      <Text>This is Phase 2 Mapping</Text>
+      <Stack style={{ flexGrow: 1 }} ref={wrapperRef}>
+        <div style={{ flexGrow: 1 }} ref={wrapperRef}>
+          {/* Heading */}
+          <MappingHeading ref={titlesRef} />
+          {/* Mapping Items */}
+          <div
+            className={classes.mappingWrapper}
+            style={{
+              height: wrapperHeight,
+            }}
+          >
+            <Group align="center" noWrap>
+              <MappingItem required={true} heading={'Geopolitics'} options={['Movies', 'World News']} />
+            </Group>
+          </div>
+        </div>
+      </Stack>
+
       <AutoImportFooter
         onNextClick={onNextClick}
         primaryButtonLoading={false}
