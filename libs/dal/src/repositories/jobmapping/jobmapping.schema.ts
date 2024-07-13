e90ecd6e8355e9e1a45feb9c3fa9998b9aefe110
Mayur <mayurkaikadi@knovator.in>
2024-07-13 15:07:57 +0530

    feat: Added schema for jobsmapping

---
+++ libs/dal/src/repositories/jobmapping/jobmapping.schema.ts
@@ -0,0 +1,34 @@
+import { model, models, Schema } from 'mongoose';
+import { schemaOptions } from '../schema-default.options';
+import { JobMappingEntity } from './jobmapping.entity';
+
+const jobMappingSchema = new Schema(
+  {
+    _id: {
+      type: Schema.Types.String,
+    },
+    key: {
+      type: Schema.Types.String,
+    },
+    name: {
+      type: Schema.Types.String,
+    },
+    isRequired: {
+      type: Schema.Types.Boolean,
+    },
+    path: {
+      type: Schema.Types.String,
+    },
+    jobId: {
+      type: Schema.Types.ObjectId,
+      ref: 'UserJob',
+    },
+  },
+  { ...schemaOptions }
+);
+
+interface IJobMapping extends JobMappingEntity, Document {
+  _id: never;
+}
+
+export const JobMapping = models.JobMapping || model<IJobMapping>('Template', jobMappingSchema);
