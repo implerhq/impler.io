14a118d10d02a8c70d712019641cb306f83fb49a
Mayur <mayurkaikadi@knovator.in>
2024-07-16 10:20:58 +0530

    feat: Took path as array of strings

--- libs/dal/src/repositories/jobmapping/jobmapping.entity.ts
+++ libs/dal/src/repositories/jobmapping/jobmapping.entity.ts
@@ -1,5 +1,5 @@
 export class JobMappingEntity {
-  _id: string;
+  _id?: string;
 
   key: string;
 
@@ -7,7 +7,7 @@ export class JobMappingEntity {
 
   isRequired: boolean;
 
-  path: string;
+  path: string[];
 
   jobId: string;
 }
