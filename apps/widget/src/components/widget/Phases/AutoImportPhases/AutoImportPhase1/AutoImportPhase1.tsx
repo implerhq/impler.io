e204d8066fcff8d40ae93b4de7267c8be86ee3bf
Mayur <mayurkaikadi@knovator.in>
2024-07-16 10:22:35 +0530

    feat: Read the RSS URL and performed necessary operations

--- apps/widget/src/components/widget/Phases/AutoImportPhases/AutoImportPhase1/AutoImportPhase1.tsx
+++ apps/widget/src/components/widget/Phases/AutoImportPhases/AutoImportPhase1/AutoImportPhase1.tsx
@@ -1,9 +1,10 @@
 import { TextInput, Text, Stack } from '@mantine/core';
-import { useForm } from 'react-hook-form';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { Container } from 'components/Common/Container';
 import { PhasesEnum } from '@types';
 import { AutoImportFooter } from 'components/Common/Footer/AutoImportFooter';
-import { validateUrl } from '@util';
+import { validateRssUrl } from '@util';
+import { useAutoImportPhase1 } from '../hooks/AutoImportPhase1/useAutoImportPhase1';
 
 interface IAutoImportPhase1Props {
   onNextClick: () => void;
@@ -14,52 +15,53 @@ interface FormValues {
 }
 
 export function AutoImportPhase1({ onNextClick }: IAutoImportPhase1Props) {
+  const { getRssXmlHeading, isLoading } = useAutoImportPhase1({
+    goNext: onNextClick,
+  });
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<FormValues>();
 
-  const onSubmit = (data: FormValues) => {
-    console.log(data);
-    onNextClick();
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    getRssXmlHeading(data.rssUrl);
   };
 
   return (
     <Container>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Stack spacing="xs" style={{ height: '100%', justifyContent: 'space-between' }}>
-          <div>
-            <Text mt="sm" ml="md">
-              RSS URL
-            </Text>
-            <TextInput
-              maw="100%"
-              placeholder="Enter the RSS URL"
-              size="md"
-              p="sm"
-              style={{
-                borderRadius: '10px',
-                borderColor: errors.rssUrl ? 'red' : undefined,
-              }}
-              {...register('rssUrl', validateUrl)}
-              error={errors.rssUrl && errors.rssUrl.message}
-            />
-            {errors.rssUrl && (
-              <Text color="red" size="sm" mt="xs" ml="md">
-                {errors.rssUrl.message}
-              </Text>
-            )}
-          </div>
-
-          <AutoImportFooter
-            onNextClick={onNextClick}
-            primaryButtonLoading={false}
-            onPrevClick={() => {}}
-            active={PhasesEnum.CONFIGURE}
+      <Stack spacing="xs" style={{ height: '100%', justifyContent: 'space-between' }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Text mt="sm" ml="md">
+            RSS URL
+          </Text>
+          <TextInput
+            maw="100%"
+            placeholder="Enter the RSS URL"
+            size="md"
+            p="sm"
+            style={{
+              borderRadius: '10px',
+              borderColor: errors.rssUrl ? 'red' : undefined,
+            }}
+            {...register('rssUrl', validateRssUrl)}
+            error={errors.rssUrl && errors.rssUrl.message}
           />
-        </Stack>
-      </form>
+          {errors.rssUrl && (
+            <Text color="red" size="sm" mt="xs" ml="md">
+              {errors.rssUrl.message}
+            </Text>
+          )}
+        </form>
+
+        <AutoImportFooter
+          onNextClick={handleSubmit(onSubmit)}
+          primaryButtonLoading={isLoading}
+          onPrevClick={() => {}}
+          active={PhasesEnum.CONFIGURE}
+        />
+      </Stack>
     </Container>
   );
 }
