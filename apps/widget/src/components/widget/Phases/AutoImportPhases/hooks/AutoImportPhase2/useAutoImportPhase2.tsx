07bfd204ba8b96af0a3b1363122ac87b296794e1
Mayur <mayurkaikadi@knovator.in>
2024-07-16 20:17:39 +0530

    feat: Custom Hook for AutoImportPhase2 added

---
+++ apps/widget/src/components/widget/Phases/AutoImportPhases/hooks/AutoImportPhase2/useAutoImportPhase2.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useMutation, useQuery } from '@tanstack/react-query';
+
+import { IErrorObject, IUserJobMapping } from '@impler/shared';
+import { useAPIState } from '@store/api.context';
+import { useJobsInfo } from '@store/jobinfo.context';
+import { notifier } from '@util';
+
+interface IUseAutoImportPhase2Props {
+  goNext: () => void;
+}
+
+export function useAutoImportPhase2({ goNext }: IUseAutoImportPhase2Props) {
+  const { api } = useAPIState();
+  const { jobsInfo } = useJobsInfo();
+  const [headings, setHeadings] = useState(
+    jobsInfo?.headings.map((heading) => ({ value: heading, label: heading, disabled: false })) || []
+  );
+  const { control, handleSubmit, getValues, reset } = useForm<{ mappings: IUserJobMapping[] }>();
+  const { isLoading: isMappingsLoading, data: mappings } = useQuery<
+    IUserJobMapping[],
+    IErrorObject,
+    IUserJobMapping[],
+    string[]
+  >(['getUserJobMappings'], () => api.getUserJobMappings(jobsInfo!._id), {
+    onSuccess(mappingsResponse) {
+      setHeadings(
+        jobsInfo.headings.map((heading) => ({
+          value: heading,
+          label: heading,
+          disabled: mappingsResponse.some((mapping) => mapping.path === heading),
+        }))
+      );
+      reset({
+        mappings: mappingsResponse.map((mapping) => ({
+          key: mapping.key,
+          name: mapping.name,
+          path: mapping.path,
+          isRequired: mapping.isRequired,
+          jobId: mapping.jobId,
+        })),
+      });
+    },
+  });
+
+  const { mutate: createJobMapping } = useMutation<IUserJobMapping[], IErrorObject, IUserJobMapping[]>(
+    (updateMappingData) => api.createUserJobMapings(jobsInfo._id, updateMappingData),
+    {
+      onSuccess() {
+        goNext();
+      },
+      onError(error) {
+        notifier.showError({ message: error.message, title: error.error });
+      },
+    }
+  );
+
+  const onSubmit = (data: any) => {
+    createJobMapping(data.mappings);
+  };
+
+  const onFieldSelect = () => {
+    const newMappings = getValues('mappings');
+    const finalizedFields = newMappings.reduce((acc: string[], mappingItem) => {
+      if (mappingItem.path) acc.push(mappingItem.path);
+
+      return acc;
+    }, []);
+    const newHeadings = headings.map((headingItem) => ({
+      ...headingItem,
+      disabled: finalizedFields.includes(headingItem.value),
+    }));
+    setHeadings(newHeadings);
+  };
+
+  return {
+    control,
+    headings,
+    mappings,
+    isColumnLoading: isMappingsLoading,
+    onSubmit: handleSubmit(onSubmit),
+    onFieldSelect,
+  };
+}
