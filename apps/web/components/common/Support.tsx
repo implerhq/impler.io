9396b5b009d8a484aafd6b4bf14b7219d036d979
chavda-bhavik <bhavikvchavda@gmail.com>
2024-07-16 20:53:43 +0530

    feat: Removed openreplay

--- apps/web/components/common/Support.tsx
+++ apps/web/components/common/Support.tsx
@@ -1,7 +1,6 @@
+import { useRef } from 'react';
 import Script from 'next/script';
 import getConfig from 'next/config';
-import Tracker from '@openreplay/tracker';
-import { useEffect, useRef } from 'react';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
@@ -12,44 +11,8 @@ interface SupportProps {
   profile?: IProfileData;
 }
 
-let tracker: Tracker;
-if (publicRuntimeConfig.NEXT_PUBLIC_OPENREPLAY_KEY) {
-  tracker = new Tracker({
-    __DISABLE_SECURE_MODE: true,
-    projectKey: publicRuntimeConfig.NEXT_PUBLIC_OPENREPLAY_KEY,
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    network: {
-      failuresOnly: true,
-      ignoreHeaders: ['Cookie'],
-      captureInIframes: true,
-      capturePayload: true,
-    },
-  });
-}
-
-export function Support({ profile }: SupportProps) {
+export function Support({}: SupportProps) {
   const twakRef = useRef<any>();
-
-  useEffect(() => {
-    if (tracker) {
-      tracker.start();
-    }
-    if (profile) {
-      if (tracker) {
-        tracker.setUserID(profile.email);
-        tracker.setMetadata('lastname', profile.lastName);
-        tracker.setMetadata('firstname', profile.firstName);
-      }
-      /*
-       * twakRef.current?.setAttributes({
-       *   id: profile._id,
-       *   name: profile.firstName,
-       *   email: profile.email,
-       * });
-       */
-    }
-  }, [profile]);
 
   return (
     <>
