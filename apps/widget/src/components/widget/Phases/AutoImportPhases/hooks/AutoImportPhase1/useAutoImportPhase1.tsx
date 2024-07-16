59785f5bdb33d27fc16d77eae50b8dd75e4fe5c0
Mayur <mayurkaikadi@knovator.in>
2024-07-16 20:21:43 +0530

    feat: AutoImportPhase1 added

---
+++ apps/widget/src/components/widget/Phases/AutoImportPhases/hooks/AutoImportPhase1/useAutoImportPhase1.tsx
@@ -0,0 +1,32 @@
+import { useMutation } from '@tanstack/react-query';
+import { useAPIState } from '@store/api.context';
+import { IErrorObject } from '@impler/shared';
+import { IUserJob } from '@impler/shared';
+import { useImplerState } from '@store/impler.context';
+import { useJobsInfo } from '@store/jobinfo.context';
+
+interface IUseAutoImportPhase1Props {
+  goNext: () => void;
+}
+
+export function useAutoImportPhase1({ goNext }: IUseAutoImportPhase1Props) {
+  const { api } = useAPIState();
+  const { templateId } = useImplerState();
+  const { setJobsInfo } = useJobsInfo();
+
+  const { isLoading, mutate: getRssXmlHeading } = useMutation<IUserJob, IErrorObject, string, [string]>(
+    ['getRssXmlHeading'],
+    (url: string) => api.getRssXmlMappingHeading(templateId!, url) as Promise<IUserJob>,
+    {
+      onSuccess(data) {
+        setJobsInfo(data);
+        goNext();
+      },
+    }
+  );
+
+  return {
+    isLoading,
+    getRssXmlHeading,
+  };
+}
