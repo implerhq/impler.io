39c25d1bebff211e1857db15d0da606a662be5eb
Mayur <mayurkaikadi@knovator.in>
2024-07-17 09:56:18 +0530

    feat: Added IsOptional class-validator for all fields

--- apps/api/src/app/import-jobs/dtos/update-jobinfo.dto.ts
+++ apps/api/src/app/import-jobs/dtos/update-jobinfo.dto.ts
@@ -1,18 +1,23 @@
-import { IsArray, IsDate, IsString } from 'class-validator';
+import { IsArray, IsDate, IsOptional, IsString } from 'class-validator';
 
 export class UpdateJobInfoDto {
   @IsString()
+  @IsOptional()
   url: string;
 
   @IsString()
+  @IsOptional()
   _templateId: string;
 
   @IsString()
+  @IsOptional()
   cron: string;
 
   @IsArray()
+  @IsOptional()
   headings: string[];
 
   @IsDate()
+  @IsOptional()
   createdOn: Date;
 }
