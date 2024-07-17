965f891fd94f9adff89317eec589e05d01e25238
Mayur <mayurkaikadi@knovator.in>
2024-07-17 09:59:17 +0530

    feat: Added usecase UpdateUserJob

--- apps/api/src/app/import-jobs/usecase/index.ts
+++ apps/api/src/app/import-jobs/usecase/index.ts
@@ -1,7 +1,7 @@
 import { CreateUserJob } from './create-userjob/create-userjob.usecase';
 import { GetJobMapping } from './get-jobmapping/get-jobmapping.usecase';
 import { CreateJobMapping } from './create-jobmapping/create-jobmapping.usecase';
-import { UpdateUserJob } from './update-cronexpression/update-userjob.usecase';
+import { UpdateUserJob } from './update-userjob/update-userjob.usecase';
 
 export const USECASES = [
   CreateUserJob,
