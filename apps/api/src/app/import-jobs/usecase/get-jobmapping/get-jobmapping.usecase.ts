6f67881eaf1e4d1f5cf7bb80ff055d1576a418b0
Mayur <mayurkaikadi@knovator.in>
2024-07-20 18:52:53 +0530

    feat: Streamlined usecase GetJobMapping

--- apps/api/src/app/import-jobs/usecase/get-jobmapping/get-jobmapping.usecase.ts
+++ apps/api/src/app/import-jobs/usecase/get-jobmapping/get-jobmapping.usecase.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { ColumnRepository, UserJobRepository, JobMappingEntity } from '@impler/dal';
+import { ColumnRepository, UserJobRepository, JobMappingEntity, JobMappingRepository } from '@impler/dal';
 
 @Injectable()
 export class GetJobMapping {
   constructor(
     private readonly userJobRepository: UserJobRepository,
-    private readonly columnRepository: ColumnRepository
+    private readonly columnRepository: ColumnRepository,
+    private readonly jobMappingRepository: JobMappingRepository
   ) {}
+
+  async getUserJobs(userId: string) {
+    return this.userJobRepository.find({ userId });
+  }
 
   async execute(_jobId: string): Promise<JobMappingEntity[]> {
     const userJob = await this.userJobRepository.findOne({ _id: _jobId });
@@ -15,6 +20,7 @@ export class GetJobMapping {
       throw new Error('User job not found');
     }
 
+    await this.jobMappingRepository.find({ _id: _jobId });
     const columns = await this.columnRepository.find({ _templateId: userJob._templateId });
     const jobsMapping = columns.map((column) => ({
       key: column.key,
