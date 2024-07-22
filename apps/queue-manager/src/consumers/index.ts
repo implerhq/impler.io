11881f955163c3adc73638aaba43403abed73894
Mayur <mayurkaikadi@knovator.in>
2024-07-22 19:16:27 +0530

    feat: Exported member send-import-job-data.consumer

--- apps/queue-manager/src/consumers/index.ts
+++ apps/queue-manager/src/consumers/index.ts
@@ -2,3 +2,4 @@ export * from './send-webhook-data.consumer';
 export * from './end-import.consumer';
 export * from './send-bubble-data.consumer';
 export * from './import-job-data.consumer';
+export * from './send-import-job-data.consumer';
