c660c65fbf5743775a81bcb16e97364cb997ecb9
Mayur <mayurkaikadi@knovator.in>
2024-07-24 16:46:36 +0530

    feat: Fixed grey background while filling the TextInput with suggested recently added urls

--- apps/widget/src/components/widget/Phases/AutoImportPhases/AutoImportPhase1/AutoImportPhase1.tsx
+++ apps/widget/src/components/widget/Phases/AutoImportPhases/AutoImportPhase1/AutoImportPhase1.tsx
@@ -1,54 +1,45 @@
 import { Stack, TextInput } from '@mantine/core';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { Container } from 'components/Common/Container';
+
 import { PhasesEnum } from '@types';
-import { AutoImportFooter } from 'components/Common/Footer/AutoImportFooter';
 import { validateRssUrl } from '@util';
+import { AutoImportFooter } from 'components/Common/Footer/AutoImportFooter';
 import { useAutoImportPhase1 } from '../hooks/AutoImportPhase1/useAutoImportPhase1';
 
 interface IAutoImportPhase1Props {
   onNextClick: () => void;
 }
 
-interface FormValues {
-  rssUrl: string;
-}
-
 export function AutoImportPhase1({ onNextClick }: IAutoImportPhase1Props) {
-  const { getRssXmlHeading, isLoading } = useAutoImportPhase1({
+  const { isLoading, register, errors, onSubmit } = useAutoImportPhase1({
     goNext: onNextClick,
   });
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>();
-
-  const onSubmit: SubmitHandler<FormValues> = (data) => {
-    getRssXmlHeading(data.rssUrl);
-  };
-
   return (
-    <Container>
-      <Stack spacing="xs" style={{ height: '100%', justifyContent: 'space-between' }}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <TextInput
-            label="RSS URL"
-            placeholder="Enter the RSS URL"
-            size="md"
-            {...register('rssUrl', validateRssUrl)}
-            error={errors.rssUrl && errors.rssUrl.message}
-          />
-        </form>
-
-        <AutoImportFooter
-          onNextClick={handleSubmit(onSubmit)}
-          primaryButtonLoading={isLoading}
-          onPrevClick={() => {}}
-          active={PhasesEnum.CONFIGURE}
+    <Stack spacing="xs" style={{ height: '100%', justifyContent: 'space-between' }}>
+      <form onSubmit={onSubmit}>
+        <TextInput
+          size="md"
+          label="RSS URL"
+          placeholder="Enter the RSS URL"
+          {...register('rssUrl', validateRssUrl)}
+          error={errors.rssUrl && errors.rssUrl.message}
+          styles={{
+            input: {
+              '&:-webkit-autofill': {
+                WebkitTextFillColor: 'black !important',
+                WebkitBoxShadow: '0 0 0 1000px white inset !important',
+              },
+            },
+          }}
         />
-      </Stack>
-    </Container>
+      </form>
+
+      <AutoImportFooter
+        onNextClick={onSubmit}
+        primaryButtonLoading={isLoading}
+        onPrevClick={() => {}}
+        active={PhasesEnum.CONFIGURE}
+      />
+    </Stack>
   );
 }
