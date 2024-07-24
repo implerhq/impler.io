4bac63f5a5109d65cb184dd53c4a8c182f6041b3
Mayur <mayurkaikadi@knovator.in>
2024-07-24 16:33:29 +0530

    feat: Implemented scheduleRssImportJob that will trigger the start cron job

--- apps/api/src/app/import-jobs/usecase/update-userjob/update-userjob.usecase.ts
+++ apps/api/src/app/import-jobs/usecase/update-userjob/update-userjob.usecase.ts
@@ -1,12 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { CronJob } from 'cron';
+import { Injectable, Logger } from '@nestjs/common';
+import { SchedulerRegistry } from '@nestjs/schedule';
+
+import { UserJobImportStatusEnum } from '@impler/shared';
 import { UserJobEntity, UserJobRepository } from '@impler/dal';
 import { UpdateUserJobCommand } from './update-userjob.command';
+import { CronJobService } from '@shared/services/cronjob.service';
 
 @Injectable()
 export class UpdateUserJob {
-  constructor(private readonly userJobRepository: UserJobRepository) {}
+  constructor(
+    private readonly cronJobService: CronJobService,
+    private readonly schedulerRegistry: SchedulerRegistry,
+    private readonly userJobRepository: UserJobRepository
+  ) {}
 
-  async execute(_id: string, data: UpdateUserJobCommand): Promise<UserJobEntity> {
-    return await this.userJobRepository.findOneAndUpdate({ _id }, data);
+  async execute(jobId: string, data: UpdateUserJobCommand): Promise<UserJobEntity> {
+    data.status = UserJobImportStatusEnum.SCHEDULING;
+    const userJob = await this.userJobRepository.findOneAndUpdate({ _id: jobId }, data);
+
+    this.scheduleRssImportJob(jobId, data.cron);
+
+    return userJob;
+  }
+
+  private scheduleRssImportJob(jobId: string, cronExpression: string) {
+    const job = new CronJob(cronExpression, () => this.cronJobService.triggerImportJob(jobId));
+    this.schedulerRegistry.addCronJob(`${jobId}_rss_import`, job);
+
+    job.start();
+
+    Logger.log(`Cron job for RSS import started with schedule: ${cronExpression}`);
   }
 }
