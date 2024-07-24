3a331b0cc992338475c53b758aaa4eb25ef469ab
Mayur <mayurkaikadi@knovator.in>
2024-07-24 16:29:21 +0530

    feat: Saved extra and externalUserId in database while creating userJob

--- apps/api/src/app/import-jobs/usecase/create-userjob/create-userjob.usecase.ts
+++ apps/api/src/app/import-jobs/usecase/create-userjob/create-userjob.usecase.ts
@@ -12,15 +12,21 @@ export class CreateUserJob {
     private readonly rssService: RSSService
   ) {}
 
-  async execute({ _templateId, url }: CreateUserJobCommand): Promise<UserJobEntity> {
+  async execute({ _templateId, url, externalUserId, extra }: CreateUserJobCommand): Promise<UserJobEntity> {
     const mimeType = await this.rssService.getMimeType(url);
     if (mimeType === FileMimeTypesEnum.XML || mimeType === FileMimeTypesEnum.TEXTXML) {
       const { rssKeyHeading } = await this.rssService.parseRssFeed(url);
+      let formattedExtra = extra || '{}';
+      try {
+        formattedExtra = JSON.parse(extra);
+      } catch (_) {}
 
       return await this.userJobRepository.create({
         url,
         headings: rssKeyHeading,
         _templateId: _templateId,
+        extra,
+        externalUserId: externalUserId || (formattedExtra as unknown as Record<string, any>)?.externalUserId,
       });
     } else {
       throw new BadRequestException(APIMessages.INVALID_RSS_URL);
