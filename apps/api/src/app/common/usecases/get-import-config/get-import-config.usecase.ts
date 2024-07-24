10e3f9756284ef0cba6bfe6ad51ac3ae7be4aaf3
Mayur <mayurkaikadi@knovator.in>
2024-07-24 16:27:59 +0530

    feat: Taking templeteId as parameter in execute method

--- apps/api/src/app/common/usecases/get-import-config/get-import-config.usecase.ts
+++ apps/api/src/app/common/usecases/get-import-config/get-import-config.usecase.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UserRepository, TemplateRepository } from '@impler/dal';
 import { IImportConfig, PaymentAPIService } from '@impler/shared';
+import { APIMessages } from '@shared/constants';
 
 @Injectable()
 export class GetImportConfig {
@@ -10,21 +11,20 @@ export class GetImportConfig {
     private templateRepository: TemplateRepository
   ) {}
 
-  async execute(projectId: string): Promise<IImportConfig> {
+  async execute(projectId: string, templateId: string): Promise<IImportConfig> {
     const userEmail = await this.userRepository.findUserEmailFromProjectId(projectId);
 
     const removeBrandingAvailable = await this.paymentAPIService.checkEvent(userEmail, 'REMOVE_BRANDING');
 
     const template = await this.templateRepository.findOne({
       _projectId: projectId,
+      ...(templateId ? { _id: templateId } : {}),
     });
 
     if (!template) {
-      throw new Error('Template not found');
+      throw new BadRequestException(APIMessages.TEMPLATE_NOT_FOUND);
     }
 
-    const importMode = template.mode;
-
-    return { showBranding: !removeBrandingAvailable, mode: importMode };
+    return { showBranding: !removeBrandingAvailable, mode: template.mode, title: template.name };
   }
 }
