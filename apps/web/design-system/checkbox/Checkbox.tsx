14d594575d28c5f680ed3e198f77f996d487d72e
Mayur <mayurkaikadi@knovator.in>
2024-08-06 13:12:38 +0530

    feat: Removed link and description

--- apps/web/design-system/checkbox/Checkbox.tsx
+++ apps/web/design-system/checkbox/Checkbox.tsx
@@ -1,6 +1,5 @@
 import { Checkbox as MantineCheckbox, MantineNumberSize } from '@mantine/core';
 import useStyles from './Checkbox.styles';
-import Link from 'next/link'; // Import Link component
 import { ReactNode } from 'react';
 
 interface CheckboxProps {
@@ -10,31 +9,10 @@ interface CheckboxProps {
   checked?: boolean;
   description?: string;
   size?: MantineNumberSize;
-  link?: string;
-  linkLabel?: string;
 }
 
-export function Checkbox({
-  label,
-  defaultChecked,
-  register,
-  checked,
-  description,
-  size,
-  link,
-  linkLabel,
-}: CheckboxProps) {
+export function Checkbox({ label, defaultChecked, register, checked, description, size }: CheckboxProps) {
   const { classes } = useStyles();
-  const combinedDescription = link ? (
-    <>
-      {description}{' '}
-      <Link href={link} target="_blank" rel="noopener noreferrer">
-        {linkLabel}
-      </Link>
-    </>
-  ) : (
-    description
-  );
 
   return (
     <MantineCheckbox
@@ -43,7 +21,7 @@ export function Checkbox({
       checked={checked}
       label={label}
       size={size}
-      description={combinedDescription}
+      description={description}
       {...register}
     />
   );
