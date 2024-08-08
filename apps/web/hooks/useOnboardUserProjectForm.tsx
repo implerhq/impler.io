ffce70cbe5113926379a1878e482f6767768b796
Mayur <mayurkaikadi@knovator.in>
2024-08-08 16:40:57 +0530

    feat: Setted _projectId from profieInfo._ProjectId using setProfileInfo

--- apps/web/hooks/useOnboardUserProjectForm.tsx
+++ apps/web/hooks/useOnboardUserProjectForm.tsx
@@ -1,11 +1,15 @@
 import { useRouter } from 'next/router';
 import { useMutation } from '@tanstack/react-query';
-import { commonApi } from '@libs/api';
+
 import { API_KEYS } from '@config';
+import { commonApi } from '@libs/api';
+import { track } from '@libs/amplitude';
+import { useAppState } from 'store/app.context';
 import { IErrorObject, IEnvironmentData } from '@impler/shared';
 
 export function useOnboardUserProjectForm() {
   const { push } = useRouter();
+  const { profileInfo, setProfileInfo } = useAppState();
 
   const { mutate: onboardUser, isLoading: isUserOnboardLoading } = useMutation<
     { onboard: IOnboardUserData; environment: IEnvironmentData },
@@ -17,6 +21,18 @@ export function useOnboardUserProjectForm() {
     (apiData) => commonApi(API_KEYS.ONBOARD_USER as any, { body: { ...apiData, onboarding: true } }),
     {
       onSuccess: () => {
+        if (profileInfo) {
+          setProfileInfo({
+            ...profileInfo,
+            _projectId: profileInfo._projectId,
+          });
+        }
+        track({
+          name: 'PROJECT CREATE',
+          properties: {
+            duringOnboard: true,
+          },
+        });
         push('/');
       },
     }
