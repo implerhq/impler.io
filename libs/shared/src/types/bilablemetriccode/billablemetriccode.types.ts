5851a42f00bab62c2d7e9a14ec5151ee7bf0a137
Mayur <mayurkaikadi@knovator.in>
2024-08-08 12:07:34 +0530

    feat: File deleted

--- libs/shared/src/types/bilablemetriccode/billablemetriccode.types.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/naming-convention
-export enum AVAILABLE_BILLABLEMETRIC_CODE_ENUM {
-  IMPORTED_ROWS = 'IMPORTED_ROWS',
-  REMOVE_BRANDING = 'REMOVE_BRANDING',
-  IMAGE_UPLOAD = 'IMAGE_UPLOAD',
-}
