f3b7624295b8c17e4ab32520541f57fb89350b37
Mayur <mayurkaikadi@knovator.in>
2024-08-10 11:11:54 +0530

    feat: Added and exported verify usecase

--- apps/api/src/app/auth/usecases/index.ts
+++ apps/api/src/app/auth/usecases/index.ts
@@ -4,17 +4,20 @@ import { RegisterUser } from './register-user/register-user.usecase';
 import { ResetPassword } from './reset-password/reset-password.usecase';
 import { RequestForgotPassword } from './request-forgot-password/request-forgot-pasword.usecase';
 
+import { Verify } from './verify/verify.usecase';
 import { LoginUserCommand } from './login-user/login-user.command';
 import { OnboardUserCommand } from './onboard-user/onboard-user.command';
 import { RegisterUserCommand } from './register-user/register-user.command';
 import { ResetPasswordCommand } from './reset-password/reset-password.command';
 import { RequestForgotPasswordCommand } from './request-forgot-password/request-forgot-pasword.command';
+
 import { CreateProject } from 'app/project/usecases';
 import { SaveSampleFile, UpdateImageColumns } from '@shared/usecases';
 import { CreateEnvironment, GenerateUniqueApiKey } from 'app/environment/usecases';
 import { CreateTemplate, UpdateCustomization, UpdateTemplateColumns } from 'app/template/usecases';
 
 export const USE_CASES = [
+  Verify,
   RegisterUser,
   LoginUser,
   OnboardUser,
@@ -31,7 +34,7 @@ export const USE_CASES = [
   //
 ];
 
-export { RegisterUser, LoginUser, RequestForgotPassword, ResetPassword, OnboardUser };
+export { Verify, RegisterUser, LoginUser, RequestForgotPassword, ResetPassword, OnboardUser };
 export {
   LoginUserCommand,
   OnboardUserCommand,
