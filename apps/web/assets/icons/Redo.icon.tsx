d413c53ea37f092b1e58928a8067b990b5c3e1a8
Mayur <mayurkaikadi@knovator.in>
2024-08-10 19:24:32 +0530

    refactor: Changed the redo icon to a ned undo icon

--- apps/web/assets/icons/Redo.icon.tsx
+++ apps/web/assets/icons/Redo.icon.tsx
@@ -6,12 +6,18 @@ export const RedoIcon = ({ size = 'sm', color }: IconType) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="-1 -2 24 24"
       width={IconSizes[size]}
       height={IconSizes[size]}
-      fill={color}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color || 'currentColor'}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
-      <path d="M19.347 7.24l.847-1.266a.984.984 0 0 1 1.375-.259c.456.31.58.93.277 1.383L19.65 10.38a.984.984 0 0 1-1.375.259L14.97 8.393a1.002 1.002 0 0 1-.277-1.382.984.984 0 0 1 1.375-.26l1.344.915C16.428 4.386 13.42 2 9.863 2c-4.357 0-7.89 3.582-7.89 8s3.533 8 7.89 8c.545 0 .987.448.987 1s-.442 1-.987 1C4.416 20 0 15.523 0 10S4.416 0 9.863 0c4.504 0 8.302 3.06 9.484 7.24z"></path>
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M19.933 13.041a8 8 0 1 1 -9.925 -8.788c3.899 -1 7.935 1.007 9.425 4.747" />
+      <path d="M20 4v5h-5" />
     </svg>
   );
 };
