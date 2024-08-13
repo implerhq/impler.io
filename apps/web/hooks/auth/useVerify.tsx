7c5339fe786b0926db970829dd301a68808599c5
Mayur <mayurkaikadi@knovator.in>
2024-08-13 20:36:04 +0530

    refactor: Resend verification otp

--- apps/web/hooks/auth/useVerify.tsx
+++ apps/web/hooks/auth/useVerify.tsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useState, useEffect, useRef } from 'react';
+import { useMutation } from '@tanstack/react-query';
 
 import { commonApi } from '@libs/api';
-import { API_KEYS } from '@config';
+import { notify } from '@libs/notify';
+import { useApp } from '@hooks/useApp';
+import { API_KEYS, NOTIFICATION_KEYS } from '@config';
 import { IErrorObject, IScreenResponse, SCREENS } from '@impler/shared';
 import { handleRouteBasedOnScreenResponse } from '@shared/helpers';
 
@@ -12,17 +14,24 @@ interface IVerifyFormData {
   otp: string;
 }
 
+const RESEND_SECONDS = 120;
+const OTP_LENGTH = 6;
+
 export function useVerify() {
   const { push } = useRouter();
-  const [error, setError] = useState<IErrorObject | undefined>(undefined);
+  const timerRef = useRef<any>();
+  const [formError, setFormError] = useState<string>();
+  const [countdown, setCountdown] = useState(RESEND_SECONDS);
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
-  const [countdown, setCountdown] = useState(120);
+  const [error, setError] = useState<IErrorObject | undefined>(undefined);
 
   const {
     control,
     handleSubmit,
     formState: {},
   } = useForm<IVerifyFormData>();
+
+  const { profile } = useApp();
 
   const { mutate: verify, isLoading: isVerificationLoading } = useMutation<
     IScreenResponse,
@@ -33,50 +42,66 @@ export function useVerify() {
       handleRouteBasedOnScreenResponse(data.screen as SCREENS, push);
     },
     onError: (errorObject: IErrorObject) => {
-      if (errorObject.error === 'OTPVerificationFalid') {
+      if (errorObject.error === 'OTPVerificationFailed') {
         setError(errorObject);
       }
     },
   });
 
-  const { refetch: resendOTP } = useQuery([API_KEYS.RESEND_OTP], () => commonApi(API_KEYS.RESEND_OTP as any, {}), {
-    enabled: false,
+  const { mutate: resendOTP } = useMutation([API_KEYS.RESEND_OTP], () => commonApi(API_KEYS.RESEND_OTP as any, {}), {
     onSuccess: () => {
-      setCountdown(120);
+      notify(NOTIFICATION_KEYS.OTP_CODE_RESENT_SUCCESSFULLY, {
+        color: 'green',
+        title: 'Verification code sent!',
+        message: (
+          <>
+            Verification code sent successully to <b>{profile?.email}</b>
+          </>
+        ),
+      });
+
+      setCountdown(RESEND_SECONDS);
       setIsButtonDisabled(true);
+      timerRef.current = setInterval(onCountDownProgress, 1000);
     },
   });
 
   const handleVerify = handleSubmit((otpFormData: IVerifyFormData) => {
+    if (!otpFormData.otp || otpFormData.otp.length !== OTP_LENGTH) {
+      setFormError('Please enter a valid 6-digit OTP');
+
+      return;
+    }
+    setFormError(undefined);
     verify(otpFormData);
   });
 
-  const handleResendCode = () => {
-    resendOTP();
+  const onCountDownProgress = () => {
+    setCountdown((prevCountdown) => {
+      if (prevCountdown === 0) {
+        setIsButtonDisabled(false); // Enable the button when countdown reaches zero
+        clearInterval(timerRef.current);
+      }
+
+      return Math.max(0, prevCountdown - 1);
+    });
   };
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setCountdown((prevCountdown) => {
-        if (prevCountdown === 0) {
-          setIsButtonDisabled(false);
-          clearInterval(timer);
+    timerRef.current = setInterval(onCountDownProgress, 1000);
 
-          return 0;
-        }
-
-        return prevCountdown - 1;
-      });
-    }, 1000);
-
-    return () => clearInterval(timer);
+    return () => {
+      if (timerRef.current) clearInterval(timerRef.current);
+    };
   }, []);
 
   return {
     control,
     handleVerify,
-    handleResendCode,
+    resendOTP,
     error,
+    formError,
+    setFormError,
     isVerificationLoading,
     isButtonDisabled,
     countdown,
