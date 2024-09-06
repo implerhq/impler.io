3de078c43bcdb31065e3308479f0773929cd7689
Mayur <mayurkaikadi@knovator.in>
2024-09-06 16:53:36 +0530

    feat: Added TeamMembersTab

---
+++ apps/web/pages/team-members/TeamMembersTab.tsx
@@ -0,0 +1,55 @@
+import { modals } from '@mantine/modals';
+import { Text } from '@mantine/core';
+import { Button } from '@ui/button';
+import { colors, MODAL_KEYS } from '@config';
+import { Members } from 'pages/team-members/Members';
+import { SentInvitations } from './SentInvitations';
+import { InviteMembersModal } from './InviteMembersModal';
+import { InvitationRequests } from './InvitationRequests';
+import { OutlinedTabs } from '@ui/OutlinedTabs';
+
+export function TeamMembersTab() {
+  const openInviteModal = () => {
+    modals.open({
+      id: MODAL_KEYS.INVITE_MEMBERS,
+      modalId: MODAL_KEYS.INVITE_MEMBERS,
+      title: (
+        <Text>
+          Invite new members in <span style={{ color: colors.yellow, fontWeight: 'bold' }}>Artha</span>
+        </Text>
+      ),
+      children: <InviteMembersModal />,
+      size: 'lg',
+      withCloseButton: true,
+    });
+  };
+
+  const tabItems = [
+    {
+      value: 'members',
+      title: 'Members',
+      badgeCount: 5,
+      content: <Members />,
+    },
+    {
+      value: 'team',
+      title: 'Sent Invitations',
+      badgeCount: 3,
+      content: <SentInvitations />,
+    },
+    {
+      value: 'invitation-requests',
+      title: 'Invitation Requests',
+      badgeCount: 10,
+      content: <InvitationRequests />,
+    },
+  ];
+
+  return (
+    <OutlinedTabs
+      defaultValue="members"
+      items={tabItems}
+      inviteButton={<Button onClick={openInviteModal}>Invite to Artha</Button>}
+    />
+  );
+}
