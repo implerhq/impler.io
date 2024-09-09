8f828008d91886cb4473fadff6be9aa9c5d4dc6f
Mayur <mayurkaikadi@knovator.in>
2024-09-09 18:36:14 +0530

    feat: Files moved or added to other location

---
+++ apps/web/pages/team-members/ProjectInvitationModal.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { Flex, Select } from '@mantine/core';
+import { Button } from '@ui/button';
+import { Controller } from 'react-hook-form';
+import { INVITATION_FORM_ROLES } from '@config';
+import { validateEmails } from '@shared/utils';
+import { useProjectInvitationForm } from '@hooks/useProjectInvitationForm';
+import { MultiSelect } from '@ui/multi-select';
+
+export function ProjectInvitationModal() {
+  const { control, handleSubmit, errors, onSubmit } = useProjectInvitationForm();
+  const [emailOptions, setEmailOptions] = useState<{ value: string; label: string }[]>([]);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="invitationEmails"
+        control={control}
+        render={({ field }) => (
+          <MultiSelect
+            label="Enter or paste one or more email addresses"
+            data={emailOptions}
+            placeholder="Select or add email addresses"
+            nothingFound="Nothing found"
+            searchable
+            creatable
+            getCreateLabel={(query) => `+ Create ${query}`}
+            onCreate={(query) => {
+              const item = { value: query, label: query };
+              setEmailOptions((current) => [...current, item]);
+
+              return item;
+            }}
+            {...field}
+            error={errors.invitationEmails ? errors.invitationEmails.message : undefined}
+            value={field.value || []} // Ensure value is an array of { value: string; label: string }
+          />
+        )}
+        rules={{
+          required: 'Email addresses are required',
+          validate: (value) => {
+            /*
+             * console.log(value);
+             * Extract the email values from the objects and validate them
+             */
+            const emailValues = value.map((item) => item);
+            console.log('EMAIL VALUES', emailValues);
+
+            return validateEmails(emailValues);
+          },
+        }}
+      />
+
+      <Controller
+        name="role"
+        control={control}
+        render={({ field }) => (
+          <Select
+            dropdownPosition="flip"
+            label="Role"
+            placeholder="Select Role"
+            data={INVITATION_FORM_ROLES}
+            mt="md"
+            {...field}
+            error={errors.role ? errors.role.message : undefined}
+          />
+        )}
+        rules={{ required: 'Role is required' }}
+      />
+
+      <Flex gap="sm" mt="lg">
+        <Button type="button" fullWidth variant="outline">
+          Cancel
+        </Button>
+        <Button type="submit" fullWidth color="blue">
+          Send Invitation(s)
+        </Button>
+      </Flex>
+    </form>
+  );
+}
--- libs/dal/src/repositories/invitation/invitation.repository.ts
+++
@@ -1,9 +0,0 @@
-import { BaseRepository } from '../base-repository';
-import { InvitationEntity } from './invitation.entity';
-import { Invitation } from './invitation.schema';
-
-export class InvitationRepository extends BaseRepository<InvitationEntity> {
-  constructor() {
-    super(Invitation, InvitationEntity);
-  }
-}
