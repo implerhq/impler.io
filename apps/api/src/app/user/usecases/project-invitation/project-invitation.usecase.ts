b31514af8b6bf5b1a18c8b4b21a1a79e84d1dd9e
Mayur <mayurkaikadi@knovator.in>
2024-09-10 12:50:22 +0530

    feat: Removed logs

--- apps/api/src/app/user/usecases/project-invitation/project-invitation.usecase.ts
+++ apps/api/src/app/user/usecases/project-invitation/project-invitation.usecase.ts
@@ -13,45 +13,34 @@ export class ProjectInvitation {
   ) {}
 
   async exec(command: ProjectInvitationCommand) {
-    const invitation = await this.projectInvitationRepository.create({
-      invitationEmails: command.invitationEmails,
-      invitedOn: new Date().toDateString(),
-      role: command.role,
-      invitedBy: command.invitatedBy,
-      _projectId: command.projectId,
-      token: randomBytes(16).toString('hex'),
-    });
-
-    let invitationUrl = `${process.env.WEB_BASE_URL}/auth/signup`;
-    invitationUrl += constructQueryString({
-      invitationId: invitation._id,
-      token: invitation.token,
-    });
-
-    if (this.emailService.isConnected()) {
-      const emailContents = this.emailService.getEmailContent({
-        type: 'PROJECT_INVITATION_EMAIL',
-        data: {
-          projectName: command.projectName,
-          invitationId: invitation._id,
-          invitationUrl,
-          invitedBy: command.invitatedBy,
-          token: invitation.token,
-          userName: command.userName,
-        },
+    for (const invitationEmail of command.invitationEmails) {
+      const invitation = await this.projectInvitationRepository.create({
+        invitationToEmail: invitationEmail,
+        invitedOn: new Date().toDateString(),
+        role: command.role,
+        invitedBy: command.invitatedBy,
+        _projectId: command.projectId,
+        token: randomBytes(16).toString('hex'),
       });
 
-      command.invitationEmails.forEach((invitationEmail) => {
-        console.log(
-          invitationEmail,
-          EMAIL_SUBJECT.PROJECT_INVITATION,
-          emailContents,
-          command.invitatedBy,
-          command.userName
-        );
+      let invitationUrl = `${process.env.WEB_BASE_URL}/auth/signup`;
+      invitationUrl += constructQueryString({
+        invitationId: invitation._id,
+        token: invitation.token,
       });
 
-      command.invitationEmails.forEach(async (invitationEmail) => {
+      if (this.emailService.isConnected()) {
+        const emailContents = this.emailService.getEmailContent({
+          type: 'PROJECT_INVITATION_EMAIL',
+          data: {
+            projectName: command.projectName,
+            invitationId: invitation._id,
+            invitationUrl,
+            invitedBy: command.invitatedBy,
+            token: invitation.token,
+            userName: command.userName,
+          },
+        });
         await this.emailService.sendEmail({
           to: invitationEmail,
           subject: EMAIL_SUBJECT.PROJECT_INVITATION,
@@ -59,7 +48,7 @@ export class ProjectInvitation {
           from: process.env.EMAIL_FROM,
           senderName: command.userName,
         });
-      });
+      }
     }
   }
 }
