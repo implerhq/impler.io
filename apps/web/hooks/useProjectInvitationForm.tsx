347162ac618d16952891578622c3b8f23bfc0eff
Mayur <mayurkaikadi@knovator.in>
2024-09-10 09:59:57 +0530

    feat: Added custom hook useProjectInvitationForm

---
+++ apps/web/hooks/useProjectInvitationForm.tsx
@@ -0,0 +1,66 @@
+import { MODAL_KEYS } from '@config';
+import { modals } from '@mantine/modals';
+import { validateEmails } from '@shared/utils';
+import { useMutation } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
+import { commonApi } from '@libs/api';
+import { API_KEYS } from '@config';
+import { IErrorObject } from '@impler/shared';
+import { useAppState } from 'store/app.context';
+
+interface ProjectInvitationData {
+  invitationEmails: string[];
+  role: string;
+  projectName: string;
+  projectId?: string;
+}
+
+export function useProjectInvitationForm() {
+  const { profileInfo } = useAppState();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ProjectInvitationData>();
+
+  const { mutate: projectInvitation, isLoading: isProjectInvitationLoading } = useMutation<
+    { projectInvitationData: ProjectInvitationData },
+    IErrorObject,
+    ProjectInvitationData
+  >(
+    [API_KEYS.PROJECT_INVITATION],
+    (apiData) =>
+      commonApi(API_KEYS.PROJECT_INVITATION as any, {
+        body: {
+          ...apiData,
+        },
+      }),
+    {
+      onSuccess: () => {
+        modals.close(MODAL_KEYS.INVITE_MEMBERS);
+      },
+      onError: () => {},
+    }
+  );
+
+  const onSubmit = (data: ProjectInvitationData) => {
+    const isValid = validateEmails(data.invitationEmails);
+    if (isValid) {
+      projectInvitation({
+        invitationEmails: data.invitationEmails,
+        role: data.role,
+        projectName: profileInfo!.projectName,
+        projectId: profileInfo?._projectId,
+      });
+    }
+  };
+
+  return {
+    control,
+    handleSubmit,
+    errors,
+    onSubmit,
+    isProjectInvitationLoading,
+  };
+}
