d526f39575d5ee779f07dd1086a4ebe305e18648
Mayur <mayurkaikadi@knovator.in>
2024-09-10 09:51:00 +0530

    feat: Added chema for prokect invitation

---
+++ libs/dal/src/repositories/project-invitation/project-invitation.schema.ts
@@ -0,0 +1,35 @@
+import { model, models, Schema } from 'mongoose';
+import { schemaOptions } from '../schema-default.options';
+import { ProjectInvitationEntity } from './project-invitation.entity';
+
+const projectInvitationSchema = new Schema(
+  {
+    invitationEmails: {
+      type: [Schema.Types.String],
+    },
+    invitedOn: {
+      type: Schema.Types.String,
+    },
+    role: {
+      type: Schema.Types.String,
+    },
+    invitedBy: {
+      type: Schema.Types.String,
+    },
+    token: {
+      type: Schema.Types.String,
+    },
+    _projectId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Project',
+    },
+  },
+  { ...schemaOptions }
+);
+
+interface IProjectInvitation extends ProjectInvitationEntity, Document {
+  _id: never;
+}
+
+export const ProjectInvitation =
+  models.ProjectInvitation || model<IProjectInvitation>('ProjectInvitation', projectInvitationSchema);
