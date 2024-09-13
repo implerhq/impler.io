ef0e804776800e36410f95a8d3fabd16b4d563a6
Mayur <mayurkaikadi@knovator.in>
2024-09-13 16:06:12 +0530

    feat: Exported member ConfirmInvitationModal

--- apps/web/components/TeamMembers/index.tsx
+++ apps/web/components/TeamMembers/index.tsx
@@ -3,4 +3,4 @@ export * from './ProjectInvitationModal';
 export * from './SentInvitations';
 export * from './TeamMembersTab';
 export * from './SentInvitationActions';
-export * from './ConformInvitationModal';
+export * from './ConfirmInvitationModal';
