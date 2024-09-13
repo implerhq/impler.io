fd219359464149cb9e4a31969f3c4502825ac149
Mayur <mayurkaikadi@knovator.in>
2024-09-13 15:49:05 +0530

    feat: FIles moved to anathor location

--- apps/api/src/app/project-invitation/usecase/project-invitation/project-invitation.command.ts
+++
@@ -1,8 +0,0 @@
-export class ProjectInvitationCommand {
-  projectName: string;
-  projectId: string;
-  invitatedBy: string;
-  invitationEmailsTo: string[];
-  role: string;
-  userName: string;
-}
