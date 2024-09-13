72eb95685a169bea350e7406ed086759af51f018
Mayur <mayurkaikadi@knovator.in>
2024-09-13 18:43:19 +0530

    feat: Added AcceptProjectInvitation usecase

--- apps/api/src/app/project-invitation/usecase/accept-project-invitation/accept-project-invitation.usecase.ts
+++ apps/api/src/app/project-invitation/usecase/accept-project-invitation/accept-project-invitation.usecase.ts
@@ -1,20 +1,39 @@
-import { ProjectInvitationRepository } from '@impler/dal';
+import { EnvironmentRepository, ProjectInvitationRepository, UserRepository, ProjectRepository } from '@impler/dal';
+import { GenerateUniqueApiKey } from 'app/environment/usecases';
 import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class GetInvitation {
-  constructor(private projectInvitationRepository: ProjectInvitationRepository) {}
+export class AcceptProjectInvitation {
+  constructor(
+    private environmentRepository: EnvironmentRepository,
 
-  async exec({ invitationId, token }: { invitationId: string; token: string }) {
-    const invitation = await this.projectInvitationRepository.findOne({
-      _id: invitationId,
-      token,
+    private projectInvitationRepository: ProjectInvitationRepository,
+
+    private projectRepository: ProjectRepository,
+
+    private userRepository: UserRepository,
+    private generateUniqueApiKey: GenerateUniqueApiKey
+  ) {}
+
+  async exec({ invitationId, token, userId }: { invitationId: string; token: string; userId: string }) {
+    const key = await this.generateUniqueApiKey.execute();
+    const invitation = await this.projectInvitationRepository.findOne({ _id: invitationId, token });
+    const environment = await this.environmentRepository.findOne({
+      _projectId: invitation._projectId,
     });
+    const projectName = (await this.projectRepository.findById(environment._projectId)).name;
 
-    if (!invitation) {
-      throw new BadRequestException('Invitation not found or token is invalid.');
+    if (!environment && invitation) {
+      throw new BadRequestException('Invitation not found.');
     }
 
-    return { email: invitation.invitationToEmail };
+    const _userId = (await this.userRepository.findById(userId))._id;
+
+    const newAddedAPIKey = await this.environmentRepository.addApiKey(environment._id, key, _userId);
+    if (newAddedAPIKey) {
+      return {
+        projectName,
+      };
+    }
   }
 }
