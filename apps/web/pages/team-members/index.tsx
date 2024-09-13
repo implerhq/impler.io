9f1ec43a5627f5385541d69d4aeb803ca58f6d05
Mayur <mayurkaikadi@knovator.in>
2024-09-13 15:50:15 +0530

    feat: Opened a modal if invitation cookie is found

--- apps/web/pages/team-members/index.tsx
+++ apps/web/pages/team-members/index.tsx
@@ -1,7 +1,24 @@
+import { useEffect } from 'react';
 import { Title } from '@mantine/core';
 import { AppLayout } from '@layouts/AppLayout';
-import { TeamMembersTab } from '@components/TeamMembers';
+import { ConfirmInvitationModal, TeamMembersTab } from '@components/TeamMembers';
+import { getCookie } from '@shared/utils';
+import { modals } from '@mantine/modals';
+import { CONSTANTS, MODAL_KEYS } from '@config';
+
 export default function TeamMembers() {
+  useEffect(() => {
+    const invitationUrlCookie = getCookie(CONSTANTS.INVITATION_URL_COOKIE);
+    if (invitationUrlCookie) {
+      modals.open({
+        title: 'Confirm Invitation?',
+        id: MODAL_KEYS.ACCEPT_INVITATION,
+        modalId: MODAL_KEYS.ACCEPT_INVITATION,
+        children: <ConfirmInvitationModal />,
+      });
+    }
+  }, []);
+
   return (
     <>
       <Title order={2}>Team Members</Title>
