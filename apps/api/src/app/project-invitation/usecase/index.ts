15617f53489d29f3bcf067eeb37703d8f040b7b8
Mayur <mayurkaikadi@knovator.in>
2024-09-13 15:47:26 +0530

    feat: Added usecases

--- apps/api/src/app/project-invitation/usecase/index.ts
+++ apps/api/src/app/project-invitation/usecase/index.ts
@@ -1,11 +1,15 @@
-import { ProjectInvitation } from './project-invitation/project-invitation.usecase';
+import { Invite } from './invite/project-invitation.usecase';
 import { SentProjectInvitations } from './sent-project-invitation/sent-project-invitation.usecase';
-import { GetInvitation } from './accept-project-invitation/accept-project-invitation.usecase';
+import { GetProjectInvitation } from './get-project-invitation/get-project-invitation.usecase';
+import { AcceptProjectInvitation } from './accept-project-invitation//accept-project-invitation.usecase';
+import { GenerateUniqueApiKey } from 'app/environment/usecases';
 
 export const USE_CASES = [
-  ProjectInvitation,
+  Invite,
   SentProjectInvitations,
-  GetInvitation,
+  GetProjectInvitation,
+  AcceptProjectInvitation,
+  GenerateUniqueApiKey,
   //
 ];
-export { ProjectInvitation, SentProjectInvitations, GetInvitation };
+export { Invite, SentProjectInvitations, GetProjectInvitation, AcceptProjectInvitation, GenerateUniqueApiKey };
