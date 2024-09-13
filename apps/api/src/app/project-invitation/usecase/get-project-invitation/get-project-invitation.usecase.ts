5053ba6d5bfab87fd005b4798ffb3c45318ed607
Mayur <mayurkaikadi@knovator.in>
2024-09-13 16:08:36 +0530

    feat: Added usecase GetProjectInvitation to fetch a particular invitation

---
+++ apps/api/src/app/project-invitation/usecase/get-project-invitation/get-project-invitation.usecase.ts
@@ -0,0 +1,20 @@
+import { ProjectInvitationRepository } from '@impler/dal';
+import { BadRequestException, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class GetProjectInvitation {
+  constructor(private projectInvitationRepository: ProjectInvitationRepository) {}
+
+  async exec({ invitationId, token }: { invitationId: string; token: string }) {
+    const invitation = await this.projectInvitationRepository.findOne({
+      _id: invitationId,
+      token,
+    });
+
+    if (!invitation) {
+      throw new BadRequestException('Invitation not found or token is invalid.');
+    }
+
+    return { email: invitation.invitationToEmail };
+  }
+}
