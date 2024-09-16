e3e7585f66e8e6c9dee2da3dec20a38730320788
Mayur <mayurkaikadi@knovator.in>
2024-09-16 18:06:42 +0530

    feat: Added AbilityContext under wrapped under AppCOntextProvider

--- apps/web/store/StoreWrapper.tsx
+++ apps/web/store/StoreWrapper.tsx
@@ -1,11 +1,19 @@
-import { PropsWithChildren } from 'react';
 import AppContextProvider from './app.context';
+import { AppAbility, defineAbilitiesFor } from 'config/defineAbilities';
+import { PropsWithChildren, useState } from 'react';
 import { PlanMetaProvider } from './planmeta.store.context';
+import { AbilityContext } from './ability.context';
 
 export function StoreWrapper({ children }: PropsWithChildren) {
+  const [ability, setAbility] = useState<AppAbility | null>(defineAbilitiesFor());
+
   return (
     <PlanMetaProvider>
-      <AppContextProvider>{children}</AppContextProvider>
+      <AppContextProvider setAbility={setAbility}>
+        {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+        {/* @ts-ignore */}
+        <AbilityContext.Provider value={ability}>{children}</AbilityContext.Provider>
+      </AppContextProvider>
     </PlanMetaProvider>
   );
 }
