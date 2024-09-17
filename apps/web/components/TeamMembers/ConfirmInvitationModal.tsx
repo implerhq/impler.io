edb0504312502704ddc67b0aa42f0edbd7334e12
Mayur <mayurkaikadi@knovator.in>
2024-09-17 18:09:48 +0530

    refactor: Changed the Messenging on the confirmation modal

--- apps/web/components/TeamMembers/ConfirmInvitationModal.tsx
+++ apps/web/components/TeamMembers/ConfirmInvitationModal.tsx
@@ -1,22 +1,43 @@
 import React from 'react';
-import { Text, Stack, Flex } from '@mantine/core';
+import { Text, Stack, Group } from '@mantine/core';
+
 import { Button } from '@ui/button';
 import { useAcceptInvitation } from '@hooks/useAcceptInvitation';
+import { colors } from '@config';
 
-export function ConfirmInvitationModal() {
-  const { acceptInvitationFromCookie, isAcceptInvitationLoading } = useAcceptInvitation();
+interface IConfirmInvitationModalProps {
+  token: string;
+  invitedBy: string;
+  projectName: string;
+  invitationId: string;
+}
+
+export function ConfirmInvitationModal({ invitationId, token, invitedBy, projectName }: IConfirmInvitationModalProps) {
+  const { onAcceptClick, isAcceptLoading } = useAcceptInvitation({
+    invitationId,
+    token,
+  });
 
   return (
     <Stack spacing="sm">
-      <Text align="center">Are you sure you want to accept this invitation?</Text>
-      <Flex gap="sm">
-        <Button variant="outline" fullWidth onClick={acceptInvitationFromCookie} loading={isAcceptInvitationLoading}>
-          Confirm
+      <Text align="center">
+        <Text span color={colors.yellow}>
+          {invitedBy}
+        </Text>{' '}
+        has invited you to join{' '}
+        <Text span color={colors.yellow}>
+          {projectName}
+        </Text>
+        . Would you like to accept the invitation?
+      </Text>
+      <Group noWrap spacing="xs">
+        <Button color="red" variant="outline" fullWidth>
+          Reject
         </Button>
-        <Button fullWidth color="blue">
-          Cancel
+        <Button fullWidth color="blue" onClick={onAcceptClick} loading={isAcceptLoading}>
+          Accept
         </Button>
-      </Flex>
+      </Group>
     </Stack>
   );
 }
