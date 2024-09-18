7029adafc6300d489fe59542eb9c96638c16849c
Mayur <mayurkaikadi@knovator.in>
2024-09-18 11:23:42 +0530

    feat: Added getInvitationData

--- libs/dal/src/repositories/project-invitation/project-invitation.repository.ts
+++ libs/dal/src/repositories/project-invitation/project-invitation.repository.ts
@@ -1,9 +1,20 @@
+import { ProjectEntity } from '../project';
 import { BaseRepository } from '../base-repository';
-import { ProjectInvitationEntity } from './project-invitation.entity';
 import { ProjectInvitation } from './project-invitation.schema';
+import { ProjectInvitationEntity } from './project-invitation.entity';
 
 export class ProjectInvitationRepository extends BaseRepository<ProjectInvitationEntity> {
   constructor() {
     super(ProjectInvitation, ProjectInvitationEntity);
+  }
+  async getInvitationData(invitationId: string, token: string) {
+    const invitation = await ProjectInvitation.findOne({ _id: invitationId, token }).populate('_projectId', 'name');
+    if (!invitation) return null;
+
+    return {
+      email: invitation.invitationToEmail,
+      invitedBy: invitation.invitedBy,
+      projectName: (invitation._projectId as unknown as ProjectEntity).name,
+    };
   }
 }
