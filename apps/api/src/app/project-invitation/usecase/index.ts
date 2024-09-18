c6e3a11e91802e94a7dc19729614a6a5c2057080
Mayur <mayurkaikadi@knovator.in>
2024-09-18 13:40:53 +0530

    feat: Added usecase ListTeamMembers

--- apps/api/src/app/project-invitation/usecase/index.ts
+++ apps/api/src/app/project-invitation/usecase/index.ts
@@ -3,6 +3,7 @@ import { SentProjectInvitations } from './sent-project-invitation/sent-project-i
 import { GetProjectInvitation } from './get-project-invitation/get-project-invitation.usecase';
 import { AcceptProjectInvitation } from './accept-project-invitation//accept-project-invitation.usecase';
 import { GenerateUniqueApiKey } from 'app/environment/usecases';
+import { ListTeamMembers } from './list-team-members/list-team-members.usecase';
 
 export const USE_CASES = [
   Invite,
@@ -10,6 +11,14 @@ export const USE_CASES = [
   GetProjectInvitation,
   AcceptProjectInvitation,
   GenerateUniqueApiKey,
+  ListTeamMembers,
   //
 ];
-export { Invite, SentProjectInvitations, GetProjectInvitation, AcceptProjectInvitation, GenerateUniqueApiKey };
+export {
+  Invite,
+  SentProjectInvitations,
+  GetProjectInvitation,
+  AcceptProjectInvitation,
+  GenerateUniqueApiKey,
+  ListTeamMembers,
+};
