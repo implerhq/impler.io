0df1441bddbcdc6a702a4bb01cdf1290accb82e9
Mayur <mayurkaikadi@knovator.in>
2024-09-18 14:34:02 +0530

    feat: Added UserRoleEnum and moved the key from IApiKeyData in the IEnvironmentData itself

--- libs/shared/src/types/environment/environment.types.ts
+++ libs/shared/src/types/environment/environment.types.ts
@@ -1,10 +1,17 @@
 export interface IApiKeyData {
-  key: string;
+  role: UserRoleEnum;
   _userId: string;
 }
 
 export interface IEnvironmentData {
   _id: string;
   _projectId: string;
+  key: string;
   apiKeys: IApiKeyData[];
+}
+
+export enum UserRoleEnum {
+  ADMIN = 'Admin',
+  TECH = 'Tech',
+  FINANCE = 'Finance',
 }
