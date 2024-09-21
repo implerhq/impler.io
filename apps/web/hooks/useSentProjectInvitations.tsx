c922a13ac5fc8fb960296e04e9d4d79acac4602c
Mayur <mayurkaikadi@knovator.in>
2024-09-21 18:11:31 +0530

    feat: Added mutation function helpers handleCopyInvitation and handleCancelInvitation

--- apps/web/hooks/useSentProjectInvitations.tsx
+++ apps/web/hooks/useSentProjectInvitations.tsx
@@ -1,9 +1,16 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { commonApi } from '@libs/api';
-import { API_KEYS } from '@config';
+import { API_KEYS, NOTIFICATION_KEYS } from '@config';
 import { IErrorObject } from '@impler/shared';
+import { useClipboard } from '@mantine/hooks';
+import { notify } from '@libs/notify';
+import { ConfirmDeleteInvitation } from '@components/TeamMembers/ConfirmDeleteInvitation';
+import { modals } from '@mantine/modals';
 
 export function useSentProjectInvitations() {
+  const queryClient = useQueryClient();
+  const clipboard = useClipboard({ timeout: 1000 });
+
   const {
     data: invitations,
     refetch: refetchInvitations,
@@ -18,6 +25,61 @@ export function useSentProjectInvitations() {
       onError() {},
     }
   );
+  const { mutate: cancelInvitation, isLoading: isCancelInvitationLoading } = useMutation<any, IErrorObject, string>(
+    (invitationId) =>
+      commonApi(API_KEYS.CANCEL_INVITATION as any, {
+        parameters: [invitationId],
+      }),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries([API_KEYS.SENT_PROJECT_INVITATION]);
+        notify(NOTIFICATION_KEYS.INVITATION_DELETED, {
+          title: 'Invitation Cancelled',
+          message: `Invitation Cancelled Successfully`,
+          color: 'green',
+        });
+      },
+      onError: (error: IErrorObject) => {
+        notify(NOTIFICATION_KEYS.ERROR_DELETING_INVITATION, {
+          title: 'Error while Deleting Invitation',
+          message: error.message || 'An Error Occured While Deleting Invitation',
+          color: 'red',
+        });
+      },
+    }
+  );
+
+  function handleCopyInvitationLink(invitationId: string) {
+    const sentInvitations = invitations?.find((invitation) => invitation._id === invitationId);
+    if (sentInvitations?._id === invitationId) {
+      clipboard.copy(sentInvitations.invitationLink);
+    }
+
+    notify(NOTIFICATION_KEYS.INVITATION_LINK_COPIED, {
+      title: 'Invitation Link Copied!',
+      message: 'Invitation Link Copied',
+      color: 'green',
+    });
+  }
+
+  function handleCancelInvitation(invitationId: string) {
+    const sentInvitation = invitations?.find((invitation) => invitation._id === invitationId);
+
+    if (sentInvitation) {
+      modals.open({
+        title: 'Confirm Invitation Cancellation',
+        children: (
+          <ConfirmDeleteInvitation
+            onDeleteConfirm={() => {
+              cancelInvitation(invitationId);
+              modals.closeAll();
+            }}
+            onCancel={() => modals.closeAll()}
+          />
+        ),
+      });
+    }
+  }
 
   return {
     invitations,
@@ -26,5 +88,9 @@ export function useSentProjectInvitations() {
     isInvitationsFetched,
     isError,
     invitationsCount: invitations?.length || 0,
+    handleCopyInvitationLink,
+    handleCancelInvitation,
+    cancelInvitation,
+    isCancelInvitationLoading,
   };
 }
