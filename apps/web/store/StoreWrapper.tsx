f970f0209b867bd4c5788d233d77c4aac9318833
Mayur <mayurkaikadi@knovator.in>
2024-09-23 16:41:49 +0530

    feat: Added AppAbility from config

--- apps/web/store/StoreWrapper.tsx
+++ apps/web/store/StoreWrapper.tsx
@@ -1,8 +1,9 @@
 import AppContextProvider from './app.context';
-import { AppAbility, defineAbilitiesFor } from 'config/defineAbilities';
+import { defineAbilitiesFor } from 'config/defineAbilities';
 import { PropsWithChildren, useState } from 'react';
 import { PlanMetaProvider } from './planmeta.store.context';
 import { AbilityContext } from './ability.context';
+import { AppAbility } from '@config';
 
 export function StoreWrapper({ children }: PropsWithChildren) {
   const [ability, setAbility] = useState<AppAbility | null>(defineAbilitiesFor());
