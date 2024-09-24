724cdbd5e743cdb58e617683e3917979c9e5a4ad
Mayur <mayurkaikadi@knovator.in>
2024-09-24 18:33:31 +0530

    feat: Sent appropriate screen

--- apps/api/src/app/project-invitation/usecase/accept-project-invitation/accept-project-invitation.usecase.ts
+++ apps/api/src/app/project-invitation/usecase/accept-project-invitation/accept-project-invitation.usecase.ts
@@ -1,6 +1,8 @@
 import { EnvironmentRepository, ProjectInvitationRepository, UserRepository, ProjectRepository } from '@impler/dal';
 import { GenerateUniqueApiKey } from 'app/environment/usecases';
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { SCREENS, UserRolesEnum } from '@impler/shared';
+import { AuthService } from 'app/auth/services/auth.service';
 
 @Injectable()
 export class AcceptProjectInvitation {
@@ -12,7 +14,8 @@ export class AcceptProjectInvitation {
     private projectRepository: ProjectRepository,
 
     private userRepository: UserRepository,
-    private generateUniqueApiKey: GenerateUniqueApiKey
+    private generateUniqueApiKey: GenerateUniqueApiKey,
+    private authService: AuthService
   ) {}
 
   async exec({ invitationId, token, userId }: { invitationId: string; token: string; userId: string }) {
@@ -27,14 +30,25 @@ export class AcceptProjectInvitation {
       throw new BadRequestException('Invitation not found.');
     }
 
-    const _userId = (await this.userRepository.findById(userId))._id;
+    const user = await this.userRepository.findById(userId);
 
-    const newAddedAPIKey = await this.environmentRepository.addApiKey(environment._id, key, _userId);
+    const newAddedAPIKey = await this.environmentRepository.addApiKey(environment._id, key, user._id);
     if (newAddedAPIKey) {
       await this.projectInvitationRepository.delete({ _id: invitationId });
 
+      const accessToken = this.authService.getSignedToken({
+        _id: user._id,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        role: invitation.role as UserRolesEnum,
+        isEmailVerified: user.isEmailVerified,
+      });
+
       return {
         projectName,
+        accessToken,
+        screen: SCREENS.HOME,
       };
     }
   }
