a4593f48b88ad9bfca711af9b5dfd0568a7e75dc
Mayur <mayurkaikadi@knovator.in>
2024-09-24 18:32:19 +0530

    feat: Removed class transformer

--- apps/api/src/app/auth/usecases/register-user/register-user.command.ts
+++ apps/api/src/app/auth/usecases/register-user/register-user.command.ts
@@ -1,20 +1,7 @@
-import { IsDefined, IsEmail, IsString } from 'class-validator';
-import { BaseCommand } from '@shared/commands/base.command';
-
-export class RegisterUserCommand extends BaseCommand {
-  @IsString()
-  @IsDefined()
+export class RegisterUserCommand {
   firstName: string;
-
-  @IsString()
-  @IsDefined()
   lastName: string;
-
-  @IsEmail()
-  @IsDefined()
   email: string;
-
-  @IsString()
-  @IsDefined()
   password: string;
+  invitationId?: string;
 }
