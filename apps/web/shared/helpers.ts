d5efbd0e22c29b4efe4890be13493341d622d563
Mayur <mayurkaikadi@knovator.in>
2024-09-24 18:40:39 +0530

    feat: Added fuction formatUrl

--- apps/web/shared/helpers.ts
+++ apps/web/shared/helpers.ts
@@ -1,13 +1,34 @@
-import { SCREENS } from '@impler/shared';
+import { constructQueryString, SCREENS } from '@impler/shared';
 import { ROUTES } from '@config';
 
-export function handleRouteBasedOnScreenResponse(screen: SCREENS, push: (url: string) => void) {
+export function formatUrl(template: string, params: any[], query: Record<string, any> = {}): string {
+  let paramIndex = 0;
+
+  const formattedRoute = template.replace(/:(\w+)/g, (match) => {
+    if (paramIndex < params.length) {
+      return String(params[paramIndex++]);
+    }
+
+    return match;
+  });
+
+  return formattedRoute + constructQueryString(query);
+}
+
+export function handleRouteBasedOnScreenResponse(
+  screen: SCREENS,
+  push: (url: string) => void,
+  parameters: string[] = []
+) {
   switch (screen) {
     case SCREENS.VERIFY:
-      push(ROUTES.OTP_VERIFY);
+      push(formatUrl(ROUTES.OTP_VERIFY, parameters));
       break;
     case SCREENS.ONBOARD:
       push(ROUTES.SIGNUP_ONBOARDING);
+      break;
+    case SCREENS.INVIATAION:
+      push(formatUrl(ROUTES.INVITATION, parameters));
       break;
     case SCREENS.HOME:
     default:
