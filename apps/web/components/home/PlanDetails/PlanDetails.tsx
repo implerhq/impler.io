7cef522d5a6efa2e2383fa9456dfd17125cddc7e
Mayur <mayurkaikadi@knovator.in>
2024-09-27 10:15:10 +0530

    feat: Added to display the Active plan for the authorised role

--- apps/web/components/home/PlanDetails/PlanDetails.tsx
+++ apps/web/components/home/PlanDetails/PlanDetails.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { modals } from '@mantine/modals';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useContext, useEffect } from 'react';
 import { Title, Text, Flex, Button, Skeleton, Stack } from '@mantine/core';
 
 import { track } from '@libs/amplitude';
 import { numberFormatter } from '@impler/shared';
 import { SelectCardModal } from '@components/settings';
 import { usePlanDetails } from '@hooks/usePlanDetails';
+import { useApp } from '@hooks/useApp';
 import { TooltipLink } from '@components/guide-point';
 import { PlansModal } from '@components/UpgradePlan/PlansModal';
 import {
@@ -18,16 +19,15 @@ import {
   DOCUMENTATION_REFERENCE_LINKS,
   ActionsEnum,
   SubjectsEnum,
+  AppAbility,
 } from '@config';
-import { defineAbilitiesFor } from 'config/defineAbilities';
-import { useApp } from '@hooks/useApp';
-import { Can } from 'store/ability.context';
+import { AbilityContext, Can } from 'store/ability.context';
 
 export function PlanDetails() {
   const router = useRouter();
 
   const { profile } = useApp();
-  const ability = defineAbilitiesFor(profile?.role);
+  useContext<AppAbility | null>(AbilityContext);
 
   const { [CONSTANTS.PLAN_CODE_QUERY_KEY]: selectedPlan, [CONSTANTS.EXPLORE_PLANS_QUERY_LEY]: explorePlans } =
     router.query;
@@ -128,61 +128,60 @@ export function PlanDetails() {
         backgroundColor: backgroundColor + '20',
       }}
     >
-      {ability.can(ActionsEnum.READ, SubjectsEnum.HOMEPAGE) && (
-        <Flex gap={5} justify="space-between" w="100%">
-          <Flex direction="column" gap={5} align="center">
-            <Title order={3} fw="bold">
-              {numberFormatter(activePlanDetails!.usage.IMPORTED_ROWS)}
-              {'/'}
-              {numberFormatter(numberOfRecords)}
-            </Title>
-            <Text size="sm" fw="bold" color={colors.TXTSecondaryDark}>
-              Records Imported
-            </Text>
-          </Flex>
-          <Flex direction="column" gap={5} align="center">
-            <Title order={3} fw="bold">
-              {activePlanDetails.plan.name}
-            </Title>
-            <Text size="sm" fw="bold" color={colors.TXTSecondaryDark}>
-              Active Plan
-            </Text>
-          </Flex>
-          {Number(activePlanDetails.plan.charge) ? (
-            <>
-              <Flex direction="column" gap={5} align="center">
-                <Title order={3} fw="bold">
-                  {'$' + activePlanDetails.plan.charge}
-                </Title>
-                <Text size="sm" fw="bold" color={colors.TXTSecondaryDark}>
-                  Outstanding Amount
-                </Text>
-              </Flex>
-            </>
-          ) : null}
-          <Flex direction="column" gap={5} align="center">
-            <Title order={3} fw="bold">
-              <>{activePlanDetails!.expiryDate}</>
-            </Title>
-            <Text size="sm" fw="bold" color={colors.TXTSecondaryDark}>
-              Expiry Date
-            </Text>
-          </Flex>
-          <Can I={ActionsEnum.BUY} a={SubjectsEnum.PLAN}>
+      <Flex gap={5} justify="space-between" w="100%">
+        <Flex direction="column" gap={5} align="center">
+          <Title order={3} fw="bold">
+            {numberFormatter(activePlanDetails!.usage.IMPORTED_ROWS)}
+            {'/'}
+            {numberFormatter(numberOfRecords)}
+          </Title>
+          <Text size="sm" fw="bold" color={colors.TXTSecondaryDark}>
+            Records Imported
+          </Text>
+        </Flex>
+        <Flex direction="column" gap={5} align="center">
+          <Title order={3} fw="bold">
+            {activePlanDetails.plan.name}
+          </Title>
+          <Text size="sm" fw="bold" color={colors.TXTSecondaryDark}>
+            Active Plan
+          </Text>
+        </Flex>
+        {Number(activePlanDetails.plan.charge) ? (
+          <>
             <Flex direction="column" gap={5} align="center">
-              <Button
-                onClick={showPlans}
-                color={isLessThanZero || activePlanDetails.usage.IMPORTED_ROWS > numberOfRecords ? 'red' : 'blue'}
-              >
-                Choose Plan
-              </Button>
-              <Text component={Link} href="/transactions" color={colors.yellow} td="underline">
-                View all transactions
+              <Title order={3} fw="bold">
+                {'$' + activePlanDetails.plan.charge}
+              </Title>
+              <Text size="sm" fw="bold" color={colors.TXTSecondaryDark}>
+                Outstanding Amount
               </Text>
             </Flex>
-          </Can>
+          </>
+        ) : null}
+        <Flex direction="column" gap={5} align="center">
+          <Title order={3} fw="bold">
+            <>{activePlanDetails!.expiryDate}</>
+          </Title>
+          <Text size="sm" fw="bold" color={colors.TXTSecondaryDark}>
+            Expiry Date
+          </Text>
         </Flex>
-      )}
+
+        <Can I={ActionsEnum.BUY} a={SubjectsEnum.PLAN}>
+          <Flex direction="column" gap={5} align="center">
+            <Button
+              onClick={showPlans}
+              color={isLessThanZero || activePlanDetails.usage.IMPORTED_ROWS > numberOfRecords ? 'red' : 'blue'}
+            >
+              Choose Plan
+            </Button>
+            <Text component={Link} href="/transactions" color={colors.yellow} td="underline">
+              View all transactions
+            </Text>
+          </Flex>
+        </Can>
+      </Flex>
 
       <TooltipLink link={DOCUMENTATION_REFERENCE_LINKS.subscriptionInformation} iconSize="md" />
     </Flex>
