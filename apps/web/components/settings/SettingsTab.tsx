d787632e25dbafa93b497de2959fa24110c12d4b
Mayur <mayurkaikadi@knovator.in>
2024-09-27 10:15:45 +0530

    feat: Added the ability from the context

--- apps/web/components/settings/SettingsTab.tsx
+++ apps/web/components/settings/SettingsTab.tsx
@@ -1,18 +1,17 @@
+import { useContext } from 'react';
 import getConfig from 'next/config';
 import { useRouter } from 'next/router';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 
 import { UserCards } from './UserCards';
-import { GenerateAccessToken } from './GenerateAccessToken';
 import { OutlinedTabs } from '@ui/OutlinedTabs';
-import { useApp } from '@hooks/useApp';
-import { defineAbilitiesFor } from 'config/defineAbilities';
-import { ActionsEnum, SubjectsEnum } from '@config';
+import { AbilityContext } from 'store/ability.context';
+import { GenerateAccessToken } from './GenerateAccessToken';
+import { ActionsEnum, AppAbility, SubjectsEnum } from '@config';
 
 export function SettingsTab() {
-  const { profile } = useApp();
-  const ability = defineAbilitiesFor(profile?.role);
+  const ability = useContext<AppAbility | null>(AbilityContext);
   const router = useRouter();
   const { publicRuntimeConfig } = getConfig();
 
@@ -21,12 +20,14 @@ export function SettingsTab() {
     loadStripe(publicRuntimeConfig.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
   const tabItems = [
-    ability.can(ActionsEnum.READ, SubjectsEnum.ACCESS_TOKEN) && {
-      value: 'accesstoken',
-      title: 'Access Token',
-      content: <GenerateAccessToken />,
-    },
+    ability &&
+      ability.can(ActionsEnum.READ, SubjectsEnum.ACCESS_TOKEN) && {
+        value: 'accesstoken',
+        title: 'Access Token',
+        content: <GenerateAccessToken />,
+      },
     stripePromise &&
+      ability &&
       ability.can(ActionsEnum.READ, SubjectsEnum.CARDS) && {
         value: 'addcard',
         title: 'Cards',
