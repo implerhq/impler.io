7456debc4c1d01b5628246a4ce72a0cfd89a0619
Mayur <mayurkaikadi@knovator.in>
2024-09-27 10:12:20 +0530

    feat: Added counting of Invitations and throwing subsequent exception message

--- apps/api/src/app/team/usecase/invite/invite.usecase.ts
+++ apps/api/src/app/team/usecase/invite/invite.usecase.ts
@@ -1,8 +1,8 @@
 import { randomBytes } from 'crypto';
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { EmailService } from '@impler/services';
 import { EMAIL_SUBJECT } from '@impler/shared';
-import { ProjectInvitationRepository } from '@impler/dal';
+import { ProjectInvitationRepository, EnvironmentRepository } from '@impler/dal';
 
 import { InviteCommand } from './invite.command';
 
@@ -10,11 +10,48 @@ import { InviteCommand } from './invite.command';
 export class Invite {
   constructor(
     private emailService: EmailService,
-    private projectInvitationRepository: ProjectInvitationRepository
+    private projectInvitationRepository: ProjectInvitationRepository,
+    private environmentRepository: EnvironmentRepository
   ) {}
 
   async exec(command: InviteCommand) {
+    const existingInvitationsCount = await this.projectInvitationRepository.count({
+      _projectId: command.projectId,
+      invitedBy: command.invitatedBy,
+    });
+
+    const totalInvitationsCount = existingInvitationsCount + command.invitationEmailsTo.length;
+    if (totalInvitationsCount > 4) {
+      throw new BadRequestException(
+        'You cannot invite more than 4 emails at a time, including already sent invitations.'
+      );
+    }
+
+    const teamMembers = await this.environmentRepository.getProjectTeamMembers(command.projectId);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const memberEmails = teamMembers.map((teamMember) => teamMember._userId.email);
+
     for (const invitationEmailTo of command.invitationEmailsTo) {
+      if (memberEmails.includes(invitationEmailTo)) {
+        throw new BadRequestException(`The email ${invitationEmailTo} is already a member of the project.`);
+      }
+
+      const existingInvitation = await this.projectInvitationRepository.findOne({
+        invitationToEmail: invitationEmailTo,
+        _projectId: command.projectId,
+      });
+
+      if (existingInvitation) {
+        throw new BadRequestException(`The email ${invitationEmailTo} has already been invited.`);
+      }
+
+      const teamMember = await this.environmentRepository.getProjectTeamMembers(command.projectId);
+      if (teamMember.length >= 4) {
+        throw new BadRequestException(
+          'You Have Already Invited More than the Allocated team members in your current Plan'
+        );
+      }
       const invitation = await this.projectInvitationRepository.create({
         invitationToEmail: invitationEmailTo,
         invitedOn: new Date().toDateString(),
