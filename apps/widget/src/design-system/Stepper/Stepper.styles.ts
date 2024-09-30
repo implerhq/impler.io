40b2545a98c0294206157021ba155618842a2766
chavda-bhavik <bhavikvchavda@gmail.com>
2024-09-30 12:34:02 +0530

    refactor: Stepper & FileDropdown

--- apps/widget/src/design-system/Stepper/Stepper.styles.ts
+++ apps/widget/src/design-system/Stepper/Stepper.styles.ts
@@ -1,16 +1,10 @@
 import { createStyles } from '@mantine/core';
-import { colors } from '../../config/colors.config';
 
-export const getSeparatorStyles = () => ({
+const getSeparatorStyles = () => ({
   backgroundColor: 'none',
   flex: 0,
   marginLeft: 7,
   marginRight: 7,
-});
-
-export const getStepLabelStyles = () => ({
-  color: colors.darkDeem,
-  fontWeight: 600,
 });
 
 export default createStyles((): Record<string, any> => {
@@ -22,6 +16,5 @@ export default createStyles((): Record<string, any> => {
       marginLeft: '5px',
     },
     separator: getSeparatorStyles(),
-    stepLabel: getStepLabelStyles(),
   };
 });
--- apps/widget/src/icons/grid.icon.tsx
+++ apps/widget/src/icons/grid.icon.tsx
@@ -11,20 +11,32 @@ export function GridIcon(props: IIcon) {
       className={props?.className}
       style={props?.styles}
     >
-      <g clip-path="url(#clip0_4259_5403)">
+      <g clipPath="url(#clip0_4259_5403)">
         <path
           d="M2.625 4.875C2.625 4.41087 2.80937 3.96575 3.13756 3.63756C3.46575 3.30937 3.91087 3.125 4.375 3.125H16.625C17.0891 3.125 17.5342 3.30937 17.8624 3.63756C18.1906 3.96575 18.375 4.41087 18.375 4.875V17.125C18.375 17.5891 18.1906 18.0342 17.8624 18.3624C17.5342 18.6906 17.0891 18.875 16.625 18.875H4.375C3.91087 18.875 3.46575 18.6906 3.13756 18.3624C2.80937 18.0342 2.625 17.5891 2.625 17.125V4.875Z"
-          stroke="white"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
           stroke-width="2"
+          d="M2.625 9.25H18.375"
+          stroke="currentColor"
           stroke-linecap="round"
           stroke-linejoin="round"
         />
-        <path d="M2.625 9.25H18.375" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-        <path d="M8.75 3.125V18.875" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        <path
+          strokeWidth="2"
+          d="M8.75 3.125V18.875"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
       </g>
       <defs>
         <clipPath id="clip0_4259_5403">
-          <rect width="21" height="21" fill="white" transform="translate(0 0.5)" />
+          <rect width="21" height="21" fill="currentColor" transform="translate(0 0.5)" />
         </clipPath>
       </defs>
     </svg>
