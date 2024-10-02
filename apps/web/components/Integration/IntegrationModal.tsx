a863d0013502f25916abcefaa6fe6fb06862a3ae
Mayur <mayurkaikadi@knovator.in>
2024-10-02 10:23:40 +0530

    feat: Passed projectId, templateId and embedScriptUrl as props

--- apps/web/components/Integration/IntegrationModal.tsx
+++ apps/web/components/Integration/IntegrationModal.tsx
@@ -1,11 +1,22 @@
+import getConfig from 'next/config';
 import { useEffect, useState } from 'react';
-import { Flex, Title } from '@mantine/core';
-import { IntegrationSelector } from './IntegrationSelector';
+import { Flex, Title, useMantineColorScheme } from '@mantine/core';
+import { colors } from '@config';
 import { IntegrationEnum } from '@impler/shared';
 import { IntegrationTabs } from './IntegrationTabs';
 import { integrationData } from './IntegrationData';
+import { IntegrationSelector } from './IntegrationSelector';
 
-export function IntegrationModal() {
+interface IIntegrationModalProps {
+  templateId: string;
+  projectId: string;
+  accessToken: string;
+}
+
+const { publicRuntimeConfig } = getConfig();
+
+export function IntegrationModal({ accessToken, projectId, templateId }: IIntegrationModalProps) {
+  const { colorScheme } = useMantineColorScheme();
   const [selectedTab, setSelectedTab] = useState<string>('Add Script');
   const [integration, setIntegration] = useState<IntegrationEnum>(IntegrationEnum.JAVASCRIPT);
 
@@ -18,7 +29,9 @@ export function IntegrationModal() {
   return (
     <>
       <Flex justify="space-between" mb="sm">
-        <Title order={3}>Integrate</Title>
+        <Title order={3} color={colorScheme === 'dark' ? colors.StrokeLight : colors.black}>
+          Integrate
+        </Title>
         <IntegrationSelector integration={integration} setIntegration={setIntegration} />
       </Flex>
 
@@ -29,7 +42,12 @@ export function IntegrationModal() {
           id: tab.toLowerCase().replace(/\s+/g, ''),
           value: tab,
           title: tab,
-          content: integrationData[integration][tab],
+          content: integrationData[integration][tab]({
+            accessToken,
+            projectId,
+            templateId,
+            embedScriptUrl: publicRuntimeConfig.NEXT_PUBLIC_EMBED_URL,
+          }),
         }))}
       />
     </>
