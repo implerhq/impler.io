5b8ee8b47b88c5c2d8a601ed8f00c9a71860d6fb
chavda-bhavik <bhavikvchavda@gmail.com>
2024-10-02 16:16:09 +0530

    feat: Rename update record

--- apps/api/src/app/review/usecases/index.ts
+++ apps/api/src/app/review/usecases/index.ts
@@ -2,7 +2,7 @@ import { PaymentAPIService } from '@impler/services';
 import { Replace } from './replace/replace.usecase';
 import { DoReview } from './do-review/do-review.usecase';
 import { DoReReview } from './do-review/re-review-data.usecase';
-import { UpdateRecord } from './update-record/update-cell.usecase';
+import { UpdateRecord } from './update-record/update-record.usecase';
 import { DeleteRecord } from './delete-record/delete-record.usecase';
 import { StartProcess } from './start-process/start-process.usecase';
 import { GetUploadData } from './get-upload-data/get-upload-data.usecase';
