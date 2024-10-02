786695d7d8a139ea0f92298cc8847703cfb6da65
chavda-bhavik <bhavikvchavda@gmail.com>
2024-10-02 16:10:39 +0530

    feat: Refactored review types

--- libs/shared/src/types/review/review.types.ts
+++ libs/shared/src/types/review/review.types.ts
@@ -1,5 +1,28 @@
+import { IRecord } from '../../entities/Record';
+
 export enum ReviewDataTypesEnum {
   'ALL' = 'all',
   'VALID' = 'valid',
   'INVALID' = 'invalid',
+}
+export interface IReviewData {
+  limit: number;
+  page: number;
+  totalPages: number;
+  totalRecords: number;
+  data: IRecord[];
+}
+
+export interface IReplaceData {
+  find?: string;
+  replace?: string;
+  column: string;
+  caseSensitive?: boolean;
+  matchEntireCell?: boolean;
+}
+
+export interface ICreateRecord {
+  startingRowIndex: number;
+
+  amountOfRows: number;
 }
