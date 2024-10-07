0a9d6a54a69347727188d704139165b5b5150b18
Mayur <mayurkaikadi@knovator.in>
2024-10-07 19:06:37 +0530

    feat: Added a close button to the modal

--- apps/web/hooks/useImportDetails.tsx
+++ apps/web/hooks/useImportDetails.tsx
@@ -96,7 +96,7 @@ export function useImportDetails({ templateId }: useImportDetailProps) {
             integrations={templateData.integration}
           />
         ),
-        withCloseButton: false,
+        withCloseButton: true,
       });
     }
   };
