b34365ad1d55a2ea069ea80916031ee6ecfab27b
Mayur <mayurkaikadi@knovator.in>
2024-10-07 13:56:59 +0530

    feat: Added suspense fallback while loading code snippet

--- apps/web/components/Integration/ContentBlock.tsx
+++ apps/web/components/Integration/ContentBlock.tsx
@@ -1,16 +1,21 @@
+import dynamic from 'next/dynamic';
 import { IntegrationLanguage } from '@config';
-import { Prism } from '@mantine/prism';
-import {} from '@mantine/prism';
+import { Suspense } from 'react';
+import { LoadingOverlay } from '@mantine/core';
 
 type CodeBlockProps = {
   code: string;
   language?: IntegrationLanguage;
 };
 
+const Prism = dynamic(() => import('@mantine/prism').then((mod) => mod.Prism));
+
 export const CodeBlock = ({ code, language = 'javascript' }: CodeBlockProps) => (
   <div style={{ position: 'relative', marginTop: 10 }}>
-    <Prism language={language} copyLabel="Copy code" copiedLabel="Copied!">
-      {code.trim()}
-    </Prism>
+    <Suspense fallback={<LoadingOverlay visible />}>
+      <Prism language={language} copyLabel="Copy code" copiedLabel="Copied!">
+        {code.trim()}
+      </Prism>
+    </Suspense>
   </div>
 );
