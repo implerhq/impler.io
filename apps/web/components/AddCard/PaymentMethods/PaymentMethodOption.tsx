3366eea30724c0f0389d140ddd9f99a4b063b2a6
Mayur <mayurkaikadi@knovator.in>
2024-10-21 19:25:57 +0530

    feat: Added refatored UI in PaymentMethodOption

--- apps/web/components/AddCard/PaymentMethods/PaymentMethodOption.tsx
+++ apps/web/components/AddCard/PaymentMethods/PaymentMethodOption.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import { colors } from '@config';
 import { capitalizeFirstLetter } from '@shared/utils';
-import { Flex, Radio, Stack, Text, useMantineTheme } from '@mantine/core';
+import { Flex, Group, Radio, Stack, Text, useMantineTheme } from '@mantine/core';
 
 interface PaymentMethodOptionProps {
   method: {
@@ -25,18 +25,34 @@ export function PaymentMethodOption({ method, selected, onChange }: PaymentMetho
   };
 
   return (
-    <label style={{ border: `1px solid ${colors.grey}`, padding: theme.spacing.xs }} onClick={handleClick}>
-      <Flex gap="xs" direction="row" align="top">
-        <Radio value={method.paymentMethodId} id={method.paymentMethodId} checked={selected} onChange={handleClick} />
-        <Stack spacing={4}>
-          <Text color={colors.grey} fw={700} size="md">
-            {capitalizeFirstLetter(method.brand)} **** {method.last4Digits}
-          </Text>
-          <Text fw="bold" color={colors.black} size="xs">
-            Expires {`${method.expMonth}/${method.expYear}`}
-          </Text>
-        </Stack>
-        <Image width={35} height={30} src={`/images/cards/${cardBrandsSrc}.png`} alt="Card Company" />
+    <label
+      style={{
+        cursor: 'pointer',
+        padding: theme.spacing.xs,
+        borderRadius: theme.spacing.xs,
+        border: `1px solid ${colors.StrokeSecondaryLight}`,
+      }}
+      onClick={handleClick}
+    >
+      <Flex gap="xs" align="top" justify="space-between">
+        <Group align="top">
+          <Radio
+            size="xs"
+            checked={selected}
+            id={method.paymentMethodId}
+            onChange={handleClick}
+            value={method.paymentMethodId}
+          />
+          <Stack spacing={4}>
+            <Text color={colors.grey} size="xs">
+              Expires {`${method.expMonth}/${method.expYear}`}
+            </Text>
+            <Text color={colors.black} fw={700} size="md">
+              {capitalizeFirstLetter(method.brand)} **** {method.last4Digits}
+            </Text>
+          </Stack>
+        </Group>
+        <Image width={40} height={30} src={`/images/cards/${cardBrandsSrc}.png`} alt="Card Company" />
       </Flex>
     </label>
   );
