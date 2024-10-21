49c21a38680fcc044a3a3a1ce1771409a0caecda
Mayur <mayurkaikadi@knovator.in>
2024-10-21 19:23:59 +0530

    feat: Added PaymentMethodForm

--- apps/web/components/AddCard/PaymentMethodForm.tsx
+++ apps/web/components/AddCard/PaymentMethodForm.tsx
@@ -1,84 +1,63 @@
+import Link from 'next/link';
 import { colors } from '@config';
+import React, { useEffect, useState } from 'react';
+import { StripeCardExpiryElementOptions } from '@stripe/stripe-js';
 import { Box, Card, Group, Stack, Title, Text } from '@mantine/core';
 import { CardNumberElement, CardExpiryElement, CardCvcElement } from '@stripe/react-stripe-js';
-import React, { useEffect, useState } from 'react';
-import { Button } from '@ui/button';
-import { notify } from '@libs/notify';
-import { NOTIFICATION_KEYS } from '@config';
-import { PaymentMethods } from './PaymentMethods';
+
 import { Coupon } from './Coupon';
-import { StripeCardExpiryElementOptions } from '@stripe/stripe-js';
+import { Button } from '@ui/button';
+import { PaymentMethods } from './PaymentMethods';
 
 interface PaymentMethodFormProps {
-  paymentMethods: any;
-  handleAddCard: () => void;
   email: string;
   planCode: string;
-  isPaymentMethodLoading: boolean;
-  selectedPaymentMethod?: string;
-  handlePaymentMethodChange: (paymentMethodId: string) => void;
-  isCouponFeatureEnabled: string;
-  appliedCouponCode?: string;
-  setAppliedCouponCode: (code?: string) => void;
-  isPurchaseLoading: boolean;
+  paymentMethods: any;
+  handleAddCard: () => void;
   handleProceed: () => void;
+  isPurchaseLoading: boolean;
+  appliedCouponCode?: string;
+  isCouponFeatureEnabled: string;
+  selectedPaymentMethod?: string;
+  isAddPaymentMethodLoading: boolean;
+  setAppliedCouponCode: (code?: string) => void;
+  handlePaymentMethodChange: (paymentMethodId: string) => void;
 }
 
+const elementOptions: StripeCardExpiryElementOptions = {
+  style: {
+    base: {
+      fontSize: '16px',
+      color: '#495057',
+      '::placeholder': {
+        color: '#6c757d',
+      },
+      backgroundColor: '#fff',
+      padding: '10px',
+    },
+    invalid: {
+      color: 'red',
+    },
+  },
+};
+
 export function PaymentMethodForm({
-  handleAddCard,
   planCode,
-  isPaymentMethodLoading,
+  handleAddCard,
+  handleProceed,
   paymentMethods,
+  isPurchaseLoading,
+  appliedCouponCode,
+  setAppliedCouponCode,
+  isCouponFeatureEnabled,
+  isAddPaymentMethodLoading,
   selectedPaymentMethod,
   handlePaymentMethodChange,
-  handleProceed,
-  isCouponFeatureEnabled,
-  isPurchaseLoading,
-  setAppliedCouponCode,
-  appliedCouponCode,
 }: PaymentMethodFormProps) {
   const [isFormVisible, setIsFormVisible] = useState(false);
-  const [isProcessing, setIsProcessing] = useState(false);
 
   const toggleFormVisibility = () => {
     setIsFormVisible((prev) => !prev);
-  };
-
-  const elementOptions: StripeCardExpiryElementOptions = {
-    style: {
-      base: {
-        fontSize: '16px',
-        color: '#495057',
-        '::placeholder': {
-          color: '#6c757d',
-        },
-        backgroundColor: '#fff',
-        padding: '10px',
-      },
-      invalid: {
-        color: 'red',
-      },
-    },
-  };
-
-  const handleAddCardAndSubscribe = async () => {
-    setIsProcessing(true);
-
-    try {
-      await handleAddCard();
-
-      if (!isPaymentMethodLoading) {
-        await handleProceed();
-      }
-    } catch (error) {
-      notify(NOTIFICATION_KEYS.ERROR_ADDING_PAYMENT_METHOD, {
-        title: 'Error during subscription!',
-        message: 'Something went wrong while processing your subscription. Please try again later.',
-        color: 'red',
-      });
-    } finally {
-      setIsProcessing(false);
-    }
   };
 
   useEffect(() => {
@@ -89,87 +68,89 @@ export function PaymentMethodForm({
 
   return (
     <Card bg={colors.white} withBorder shadow="sm" w="50%" radius={0}>
-      <Stack spacing="md" style={{ height: '100%' }}>
-        <Group position="apart" mb="md">
-          <Title fw="bold" order={3}>
+      <Stack spacing="md" h="100%">
+        <Group position="apart">
+          <Title color={colors.black} fw="bold" order={3}>
             Payment Method
           </Title>
-          <Button onClick={toggleFormVisibility}>+ Add New Card</Button>
+          <Link href="#" onClick={toggleFormVisibility}>
+            {isFormVisible ? 'Show Added Cards' : '+ Add New Card'}
+          </Link>
         </Group>
 
-        <PaymentMethods
-          paymentMethods={paymentMethods}
-          selectedPaymentMethod={selectedPaymentMethod}
-          handlePaymentMethodChange={handlePaymentMethodChange}
-        />
-
-        {isCouponFeatureEnabled === 'true' && (
-          <Coupon planCode={planCode} couponCode={appliedCouponCode} setAppliedCouponCode={setAppliedCouponCode} />
-        )}
-
-        {isFormVisible && (
-          <Stack spacing="md" my="md">
-            <Box>
-              <Text size="sm" weight={500} color="#333">
-                Card Number
-              </Text>
-              <Box
-                sx={{
-                  border: '1px solid #ced4da',
-                  borderRadius: '4px',
-                  padding: '8px',
-                  backgroundColor: '#fff',
-                }}
-              >
-                <CardNumberElement options={elementOptions} />
-              </Box>
-            </Box>
-
-            <Group grow>
+        <Stack spacing="md" style={{ flexGrow: 1 }}>
+          {isFormVisible ? (
+            <Stack spacing="md">
               <Box>
                 <Text size="sm" weight={500} color="#333">
-                  Expiry Date
+                  Card Number
                 </Text>
                 <Box
                   sx={{
                     border: '1px solid #ced4da',
+                    borderRadius: '4px',
                     padding: '8px',
                     backgroundColor: '#fff',
                   }}
                 >
-                  <CardExpiryElement options={elementOptions} />
+                  <CardNumberElement options={elementOptions} />
                 </Box>
               </Box>
 
-              <Box>
-                <Text size="sm" weight={500} color="#333">
-                  CVV
-                </Text>
-                <Box
-                  sx={{
-                    border: '1px solid #ced4da',
-                    padding: '8px',
-                    backgroundColor: '#fff',
-                  }}
-                >
-                  <CardCvcElement options={elementOptions} />
+              <Group grow>
+                <Box>
+                  <Text size="sm" weight={500} color="#333">
+                    Expiry Date
+                  </Text>
+                  <Box
+                    sx={{
+                      border: '1px solid #ced4da',
+                      padding: '8px',
+                      backgroundColor: '#fff',
+                    }}
+                  >
+                    <CardExpiryElement options={elementOptions} />
+                  </Box>
                 </Box>
-              </Box>
-            </Group>
-          </Stack>
-        )}
 
-        <Box mt="auto">
-          <Button
-            loading={isProcessing || isPaymentMethodLoading || isPurchaseLoading}
-            onClick={handleAddCardAndSubscribe}
-            fullWidth
-            color="blue"
-            size="md"
-          >
-            Subscribe
-          </Button>
-        </Box>
+                <Box>
+                  <Text size="sm" weight={500} color="#333">
+                    CVV
+                  </Text>
+                  <Box
+                    sx={{
+                      border: '1px solid #ced4da',
+                      padding: '8px',
+                      backgroundColor: '#fff',
+                    }}
+                  >
+                    <CardCvcElement options={elementOptions} />
+                  </Box>
+                </Box>
+              </Group>
+            </Stack>
+          ) : (
+            <PaymentMethods
+              paymentMethods={paymentMethods}
+              selectedPaymentMethod={selectedPaymentMethod}
+              handlePaymentMethodChange={handlePaymentMethodChange}
+            />
+          )}
+
+          {isCouponFeatureEnabled === 'true' && (
+            <Coupon planCode={planCode} couponCode={appliedCouponCode} setAppliedCouponCode={setAppliedCouponCode} />
+          )}
+        </Stack>
+
+        <Button
+          loading={isAddPaymentMethodLoading || isPurchaseLoading}
+          onClick={isFormVisible ? handleAddCard : handleProceed}
+          fullWidth
+          color="blue"
+          size="md"
+        >
+          Subscribe
+        </Button>
       </Stack>
     </Card>
   );
