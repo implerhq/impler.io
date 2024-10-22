f48683fbf94caa66a8591f9ab9af5c8dcc4c1b39
Mayur <mayurkaikadi@knovator.in>
2024-10-22 19:02:27 +0530

    feat: Corrected the clip-path to clipPath

--- apps/web/assets/icons/BackArrow.icon.tsx
+++ apps/web/assets/icons/BackArrow.icon.tsx
@@ -10,7 +10,7 @@ export const BackArrowIcon = ({ size = 'lg' }: IconType) => {
       viewBox="0 0 24 24"
       fill="none"
     >
-      <g clip-path="url(#clip0_4907_2321)">
+      <g clipPath="url(#clip0_4907_2321)">
         <path d="M5 12H19" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
         <path d="M5 12L9 16" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
         <path d="M5 12L9 8" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
