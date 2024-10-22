df0613be606eaaad804f078fd2f6da2e6a1c9bce
Mayur <mayurkaikadi@knovator.in>
2024-10-22 18:47:06 +0530

    feat: Streamlined UI as per designs

--- apps/web/components/home/PlanDetails/CancelSubscriptionModal.tsx
+++ apps/web/components/home/PlanDetails/CancelSubscriptionModal.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Checkbox, Flex, Text, Center } from '@mantine/core';
+import { Checkbox, Flex, Text, Center, Stack } from '@mantine/core';
 import { Button } from '@ui/button';
 import { useForm, Controller } from 'react-hook-form';
 import { useCancelPlan } from '@hooks/useCancelPlan';
 import { useAppState } from 'store/app.context';
 import { modals } from '@mantine/modals';
 import { PlanCancelDetails } from './illustrations/PlanCancelDetails';
-import { MEMBERSHIP_CANCELLATION_REASONS } from '@config';
+import { colors, MEMBERSHIP_CANCELLATION_REASONS } from '@config';
 
 type FormData = {
   reasons: string[];
@@ -27,50 +27,51 @@ export function CancelSubscriptionModal() {
 
   return (
     <>
-      <Text size="xl" weight={700} align="center">
-        Cancel Subscription
-      </Text>
-
-      <Center>
-        <PlanCancelDetails />
-      </Center>
-
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Text weight={500} mb="xs">
-          Reasons
+      <Stack spacing="lg">
+        <Text color={colors.white} size="md" weight={700} align="center">
+          Cancel Subscription
         </Text>
-        <Flex direction="column" gap="xs">
-          {MEMBERSHIP_CANCELLATION_REASONS.map((reason) => (
-            <Controller
-              key={reason}
-              name="reasons"
-              control={control}
-              render={({ field }) => (
-                <Checkbox
-                  label={reason}
-                  value={reason}
-                  checked={field.value.includes(reason)}
-                  onChange={() => {
-                    const newReasons = field.value.includes(reason)
-                      ? field.value.filter((resn) => resn !== reason)
-                      : [...field.value, reason];
-                    field.onChange(newReasons);
-                  }}
-                />
-              )}
-            />
-          ))}
-        </Flex>
 
-        <Flex mt="xl" gap="md">
-          <Button onClick={() => modals.closeAll()} fullWidth variant="outline">
-            Close
-          </Button>
-          <Button type="submit" loading={isCancelPlanLoading} fullWidth>
-            Cancel Subscription
-          </Button>
-        </Flex>
-      </form>
+        <Center>
+          <PlanCancelDetails />
+        </Center>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Flex direction="column" gap="xs">
+            <Text color={colors.white} weight={600}>
+              Reasons
+            </Text>
+            {MEMBERSHIP_CANCELLATION_REASONS.map((reason) => (
+              <Controller
+                key={reason}
+                name="reasons"
+                control={control}
+                render={({ field }) => (
+                  <Checkbox
+                    label={<Text color={colors.white}>{reason}</Text>}
+                    value={reason}
+                    checked={field.value.includes(reason)}
+                    onChange={() => {
+                      const newReasons = field.value.includes(reason)
+                        ? field.value.filter((resn) => resn !== reason)
+                        : [...field.value, reason];
+                      field.onChange(newReasons);
+                    }}
+                  />
+                )}
+              />
+            ))}
+          </Flex>
+
+          <Flex mt="sm" gap="md">
+            <Button onClick={() => modals.closeAll()} fullWidth variant="outline">
+              Close
+            </Button>
+            <Button type="submit" loading={isCancelPlanLoading} fullWidth>
+              Cancel Subscription
+            </Button>
+          </Flex>
+        </form>
+      </Stack>
     </>
   );
 }
