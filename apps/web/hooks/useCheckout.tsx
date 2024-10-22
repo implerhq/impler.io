139536e6e1cd57528d9fddf7b578c1f18b910b9e
Mayur <mayurkaikadi@knovator.in>
2024-10-22 18:48:43 +0530

    feat: Streamlined useCheckout hook

--- apps/web/hooks/useCheckout.tsx
+++ apps/web/hooks/useCheckout.tsx
@@ -1,30 +1,26 @@
 import { API_KEYS } from '@config';
 import { commonApi } from '@libs/api';
 import { IErrorObject } from '@impler/shared';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 interface UseCheckoutProps {
   planCode: string;
   couponCode?: string;
-  paymentMethodId?: string;
 }
 
-export function useCheckout({ couponCode, planCode, paymentMethodId }: UseCheckoutProps) {
-  const { data: checkoutData, isLoading: isCheckoutDataLoading } = useQuery<
-    unknown,
-    IErrorObject,
-    ICheckoutData,
-    (string | undefined)[]
-  >(
-    [API_KEYS.CHECKOUT, couponCode, planCode, paymentMethodId],
-    () => commonApi(API_KEYS.CHECKOUT as any, { query: { couponCode, planCode, paymentMethodId } }),
-    {
-      enabled: !!planCode,
-    }
+export function useCheckout({ couponCode, planCode }: UseCheckoutProps) {
+  const {
+    mutate: getCheckoutData,
+    data: checkoutData,
+    isLoading: isCheckoutDataLoading,
+  } = useMutation<ICheckoutData, IErrorObject, string | undefined, (string | undefined)[]>(
+    [API_KEYS.CHECKOUT, couponCode, planCode],
+    (paymentMethodId) => commonApi(API_KEYS.CHECKOUT as any, { query: { couponCode, planCode, paymentMethodId } })
   );
 
   return {
     checkoutData,
+    getCheckoutData,
     isCheckoutDataLoading,
   };
 }
