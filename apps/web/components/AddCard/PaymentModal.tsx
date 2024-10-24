c52b8d871be932a4a5de8fa2f25ff9eb748af68c
Mayur <mayurkaikadi@knovator.in>
2024-10-24 18:43:31 +0530

    feat: Added PaymentModal

---
+++ apps/web/components/AddCard/PaymentModal.tsx
@@ -0,0 +1,52 @@
+import getConfig from 'next/config';
+import React, { useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+import { MODAL_KEYS } from '@config';
+
+import { modals } from '@mantine/modals';
+import { ChangeCardModalContent } from './ChangeCardModalContent';
+import { SubscribeToPlan } from './SubscribeToPlan';
+
+const { publicRuntimeConfig } = getConfig();
+
+const stripePromise =
+  publicRuntimeConfig.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY &&
+  loadStripe(publicRuntimeConfig.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
+export interface PaymentModalProps {
+  email: string;
+  planCode: string;
+  onClose: () => void;
+  modalId: string;
+}
+
+export function PaymentModal(props: PaymentModalProps) {
+  useEffect(() => {
+    const modalId = props.modalId;
+    modals.open({
+      modalId,
+      size: 'calc(70vw - 40px)',
+      withCloseButton: false,
+      centered: true,
+      styles: {
+        body: {
+          padding: 0,
+        },
+      },
+      children:
+        modalId === MODAL_KEYS.SELECT_CARD ? (
+          <Elements stripe={stripePromise}>
+            <SubscribeToPlan {...props} />
+          </Elements>
+        ) : (
+          <Elements stripe={stripePromise}>
+            <ChangeCardModalContent {...props} />
+          </Elements>
+        ),
+    });
+  }, [props.modalId, props]);
+
+  return null;
+}
