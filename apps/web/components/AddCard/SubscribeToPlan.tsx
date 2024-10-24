697e081376a8aefd7fa9c96697f26678e8c58f87
Mayur <mayurkaikadi@knovator.in>
2024-10-24 18:44:41 +0530

    feat: Renamed to SubscribeToPlan

---
+++ apps/web/components/AddCard/SubscribeToPlan.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { CardNumberElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { Flex, Loader } from '@mantine/core';
+
+import { useCheckout } from '@hooks/useCheckout';
+import { useSubscribe } from '@hooks/useSubscribe';
+
+import { useAddCard } from '@hooks/useAddCard';
+import { CheckoutContent } from './CheckoutContent';
+import { PaymentMethodForm } from './PaymentMethodForm';
+import { usePaymentMethods } from '@hooks/usePaymentMethods';
+
+export interface SelectCardModalContentProps {
+  email: string;
+  planCode: string;
+  onClose: () => void;
+}
+
+export function SubscribeToPlan({ email, planCode }: SelectCardModalContentProps) {
+  const { refetchPaymentMethods, paymentMethods, isPaymentMethodsLoading } = usePaymentMethods();
+
+  const {
+    handleProceed,
+    appliedCouponCode,
+    isPurchaseLoading,
+    setAppliedCouponCode,
+    isCouponFeatureEnabled,
+    isPaymentMethodsFetching,
+  } = useSubscribe({
+    email,
+    planCode,
+  });
+
+  const { getCheckoutData, checkoutData, isCheckoutDataLoading } = useCheckout({
+    couponCode: appliedCouponCode,
+    planCode,
+  });
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const { addPaymentMethod, isAddPaymentMethodLoading, setIsPaymentMethodLoading } = useAddCard({
+    refetchPaymentMethods,
+    handleProceed,
+  });
+
+  const handleAddCard = async () => {
+    if (!stripe || !elements) return;
+
+    const cardNumberElement = elements.getElement(CardNumberElement);
+
+    if (!cardNumberElement) return;
+
+    setIsPaymentMethodLoading(true);
+    const { paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: cardNumberElement,
+    });
+
+    if (paymentMethod) {
+      addPaymentMethod(paymentMethod.id);
+    }
+  };
+
+  return (
+    <>
+      {isPaymentMethodsLoading || isPaymentMethodsFetching ? (
+        <Flex align="center" justify="center" style={{ minHeight: '200px' }}>
+          <Loader />
+        </Flex>
+      ) : (
+        <Flex gap={0}>
+          <CheckoutContent checkoutData={checkoutData} isCheckoutDataLoading={isCheckoutDataLoading} />
+
+          <PaymentMethodForm
+            email={email}
+            planCode={planCode}
+            handleAddCard={handleAddCard}
+            handleProceed={handleProceed}
+            paymentMethods={paymentMethods}
+            getCheckoutData={getCheckoutData}
+            appliedCouponCode={appliedCouponCode}
+            isPurchaseLoading={isPurchaseLoading}
+            setAppliedCouponCode={setAppliedCouponCode}
+            isCouponFeatureEnabled={isCouponFeatureEnabled}
+            isAddPaymentMethodLoading={isAddPaymentMethodLoading}
+          />
+        </Flex>
+      )}
+    </>
+  );
+}
