b5881b0700281d1057fa46428eb803f54bb09c58
Mayur <mayurkaikadi@knovator.in>
2024-10-25 15:43:03 +0530

    feat: Steamlined the hook to have better error handling

--- apps/web/hooks/useUpdatePaymentMethod.tsx
+++ apps/web/hooks/useUpdatePaymentMethod.tsx
@@ -1,31 +1,29 @@
 import { useMutation } from '@tanstack/react-query';
-import { useState } from 'react';
 import { commonApi } from '@libs/api';
 import { API_KEYS, NOTIFICATION_KEYS } from '@config';
 import { IErrorObject } from '@impler/shared';
 import { notify } from '@libs/notify';
 
 export function useUpdatePaymentMethod() {
-  const [updatedPaymentMethodId, setUpdatedPaymentMethodId] = useState<string | undefined>(undefined);
-
   const { mutate: updatePaymentMethod, isLoading: isUpdatePaymentMethodLoading } = useMutation<
     any,
     IErrorObject,
     { paymentMethodId: string; email: string }
   >(
-    [API_KEYS.UPDATE_PAYMENT_METHOD],
+    [API_KEYS.UPDATE_SUBSCRIPTION_PAYMENT_METHOD],
     async ({ paymentMethodId, email }) =>
-      commonApi(API_KEYS.UPDATE_PAYMENT_METHOD as any, {
+      commonApi(API_KEYS.UPDATE_SUBSCRIPTION_PAYMENT_METHOD as any, {
         parameters: [paymentMethodId, email],
       }),
     {
-      onSuccess() {
-        setUpdatedPaymentMethodId(updatedPaymentMethodId);
-        notify(NOTIFICATION_KEYS.PAYMENT_METHOD_UPDATED, {
-          title: 'Payment Method Updated',
-          message: 'Your payment method has been successfully updated.',
-          color: 'green',
-        });
+      onSuccess(data) {
+        if (data.success) {
+          notify(NOTIFICATION_KEYS.PAYMENT_METHOD_UPDATED, {
+            title: 'Payment Method Updated',
+            message: data.message,
+            color: 'green',
+          });
+        }
       },
       onError(error: IErrorObject) {
         notify(NOTIFICATION_KEYS.ERROR_UPDATING_PAYMENT_METHOD, {
@@ -40,6 +38,5 @@ export function useUpdatePaymentMethod() {
   return {
     updatePaymentMethod,
     isUpdatePaymentMethodLoading,
-    updatedPaymentMethodId,
   };
 }
