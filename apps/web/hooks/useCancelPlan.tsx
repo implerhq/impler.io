8644be1527f935653f8346db88668dc492ecb6c8
Mayur <mayurkaikadi@knovator.in>
2024-11-06 16:10:59 +0530

    feat: Refeteching subscription on Cancel Plan

--- apps/web/hooks/useCancelPlan.tsx
+++ apps/web/hooks/useCancelPlan.tsx
@@ -1,33 +1,37 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { commonApi } from '@libs/api';
 import { API_KEYS, MODAL_KEYS, NOTIFICATION_KEYS } from '@config';
-import { IErrorObject } from '@impler/shared';
+import { IErrorObject, ISubscriptionData } from '@impler/shared';
 import { modals } from '@mantine/modals';
 import { notify } from '@libs/notify';
 import { CancelSubscriptionModal } from '@components/home/PlanDetails/CancelSubscriptionModal';
 
 interface UseCancelPlanProps {
   email: string;
+  projectId?: string;
 }
 
-export function useCancelPlan({ email }: UseCancelPlanProps) {
+export function useCancelPlan({ email, projectId }: UseCancelPlanProps) {
   const queryClient = useQueryClient();
+
   const { mutate: cancelPlan, isLoading: isCancelPlanLoading } = useMutation<
-    unknown,
+    ISubscriptionData,
     IErrorObject,
     void,
     [string, string]
   >([API_KEYS.CANCEL_SUBSCRIPTION, email], () => commonApi(API_KEYS.CANCEL_SUBSCRIPTION as any, {}), {
-    onSuccess(data: any) {
-      queryClient.invalidateQueries([API_KEYS.FETCH_ACTIVE_SUBSCRIPTION, email]);
+    onSuccess(data) {
+      queryClient.invalidateQueries([API_KEYS.FETCH_ACTIVE_SUBSCRIPTION, projectId]);
+
       modals.close(MODAL_KEYS.PAYMENT_PLANS);
+      modals.closeAll();
+
       notify(NOTIFICATION_KEYS.MEMBERSHIP_CANCELLED, {
         title: 'Subscription Cancelled',
         message: `Your subscription is cancelled.
-         Your current subscription will continue till ${data.expiryDate}. You won't be charged again.`,
+           Your current subscription will continue till ${data.expiryDate}. You won't be charged again.`,
         color: 'red',
       });
-      modals.closeAll();
     },
   });
 
