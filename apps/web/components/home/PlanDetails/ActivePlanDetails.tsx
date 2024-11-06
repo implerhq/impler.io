d294259327eb930ab674daf12cdbeb8e6418ebae
Mayur <mayurkaikadi@knovator.in>
2024-11-06 16:09:02 +0530

    feat: Added proper styling

--- apps/web/components/home/PlanDetails/ActivePlanDetails.tsx
+++ apps/web/components/home/PlanDetails/ActivePlanDetails.tsx
@@ -1,13 +1,24 @@
-import { TooltipLink } from '@components/guide-point';
-import { DOCUMENTATION_REFERENCE_LINKS, MODAL_KEYS, PLANCODEENUM } from '@config';
-import { ISubscriptionData, numberFormatter } from '@impler/shared';
-import { Stack, Group, Divider, Title } from '@mantine/core';
-import { Button } from '@ui/button';
-import Link from 'next/link';
 import React from 'react';
+import Link from 'next/link';
+import dayjs from 'dayjs';
+import { TooltipLink } from '@components/guide-point';
+import {
+  ActionsEnum,
+  DATE_FORMATS,
+  DOCUMENTATION_REFERENCE_LINKS,
+  MODAL_KEYS,
+  PLANCODEENUM,
+  SubjectsEnum,
+} from '@config';
+import { ISubscriptionData, numberFormatter } from '@impler/shared';
+import { Stack, Group, Divider, Title, Alert } from '@mantine/core';
+import { Button } from '@ui/button';
 import { PlanDetailCard } from './PlanDetailsCard';
 import { useCancelPlan } from '@hooks/useCancelPlan';
 import { usePlanDetails } from '@hooks/usePlanDetails';
+import { InformationIcon } from '@assets/icons/Information.icon';
+import { useAlertStyles } from './ActivePlanDetails.styles';
+import { Can } from 'store/ability.context';
 
 interface ActivePlanDetailsProps {
   activePlanDetails: ISubscriptionData;
@@ -15,6 +26,7 @@ interface ActivePlanDetailsProps {
   showWarning?: boolean;
   email?: string;
   projectId?: string;
+  projectName?: string;
   showPlans: () => void;
 }
 
@@ -26,11 +38,11 @@ export function ActivePlanDetails({
   email,
   projectId,
 }: ActivePlanDetailsProps) {
-  const { openCancelPlanModal } = useCancelPlan({ email: email as string });
+  const { classes } = useAlertStyles();
+  const { openCancelPlanModal } = useCancelPlan({ email: email as string, projectId });
   const { onOpenPaymentModal } = usePlanDetails({
     projectId: projectId!,
   });
-  const openCancelModal = () => openCancelPlanModal();
 
   return (
     <Stack>
@@ -40,7 +52,7 @@ export function ActivePlanDetails({
           <Button component={Link} href="/transactions" variant="filled">
             View all transactions
           </Button>
-          {!(activePlanDetails.plan.code === PLANCODEENUM.STARTER || activePlanDetails.plan.canceledOn) ? (
+          {!(activePlanDetails.plan.code === PLANCODEENUM.STARTER || activePlanDetails.plan.canceledOn) && (
             <Button
               onClick={() => {
                 onOpenPaymentModal({
@@ -53,7 +65,7 @@ export function ActivePlanDetails({
             >
               Change Card
             </Button>
-          ) : null}
+          )}
         </Group>
       </Group>
 
@@ -74,19 +86,33 @@ export function ActivePlanDetails({
 
         <Divider />
 
-        <Group spacing="xs">
-          <Button onClick={showPlans} variant="filled">
-            Upgrade Plan
-          </Button>
-
-          {activePlanDetails?.plan.code != PLANCODEENUM.STARTER && (
-            <Button color="red" variant="outline" onClick={openCancelModal}>
-              Cancel Plan
+        <Can I={ActionsEnum.BUY} a={SubjectsEnum.PLAN}>
+          <Group spacing="xs">
+            <Button onClick={showPlans} variant="filled">
+              Upgrade Plan
             </Button>
-          )}
 
-          <TooltipLink link={DOCUMENTATION_REFERENCE_LINKS.subscriptionInformation} iconSize="md" />
-        </Group>
+            {activePlanDetails.plan.canceledOn ? (
+              <Alert icon={<InformationIcon size="md" />} variant="filled" classNames={classes}>
+                Your Plan cancellation on {dayjs(activePlanDetails.plan.canceledOn).format(DATE_FORMATS.LONG)} and
+                Expire on {dayjs(activePlanDetails.expiryDate).format(DATE_FORMATS.LONG)}
+              </Alert>
+            ) : (
+              activePlanDetails.plan.code !== PLANCODEENUM.STARTER && (
+                <Button
+                  color="red"
+                  variant="outline"
+                  onClick={openCancelPlanModal}
+                  leftIcon={<InformationIcon size="md" />}
+                >
+                  Cancel Plan
+                </Button>
+              )
+            )}
+
+            <TooltipLink link={DOCUMENTATION_REFERENCE_LINKS.subscriptionInformation} iconSize="md" />
+          </Group>
+        </Can>
       </Stack>
     </Stack>
   );
