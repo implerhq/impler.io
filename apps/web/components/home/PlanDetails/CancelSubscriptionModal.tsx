06d33781f15ed4d238eeb6a726650997509a0149
Mayur <mayurkaikadi@knovator.in>
2024-11-07 17:48:03 +0530

    feat: Handled error message on the modal if user does not select any reason

--- apps/web/components/home/PlanDetails/CancelSubscriptionModal.tsx
+++ apps/web/components/home/PlanDetails/CancelSubscriptionModal.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { Checkbox, Flex, Text, Center, Stack } from '@mantine/core';
 import { Button } from '@ui/button';
 import { Controller } from 'react-hook-form';
-import { modals } from '@mantine/modals';
 import { PlanCancelDetails } from './illustrations/PlanCancelDetails';
-import { colors } from '@config';
-import { MEMBERSHIP_CANCELLATION_REASONS } from '@impler/shared';
+import { colors, MEMBERSHIP_CANCELLATION_REASONS } from '@config';
 
 interface CancelSubscriptionModalProps {
   control: any;
   handleSubmit: any;
   isCancelPlanLoading: boolean;
+  errors: any;
+  onClose: () => void;
 }
 
-export function CancelSubscriptionModal({ control, handleSubmit, isCancelPlanLoading }: CancelSubscriptionModalProps) {
+export function CancelSubscriptionModal({
+  control,
+  handleSubmit,
+  isCancelPlanLoading,
+  onClose,
+}: CancelSubscriptionModalProps) {
   return (
     <Stack spacing="lg">
       <Text color={colors.white} size="md" weight={700} align="center">
@@ -29,32 +34,43 @@ export function CancelSubscriptionModal({ control, handleSubmit, isCancelPlanLoa
           <Text color={colors.white} weight={600}>
             Reasons
           </Text>
-          {MEMBERSHIP_CANCELLATION_REASONS.map((reason) => (
-            <Controller
-              key={reason}
-              name="reasons"
-              control={control}
-              render={({ field }) => (
-                <Checkbox
-                  label={<Text color="white">{reason}</Text>}
-                  value={reason}
-                  checked={Array.isArray(field.value) && field.value.includes(reason)}
-                  onChange={() => {
-                    const newReasons = Array.isArray(field.value)
-                      ? field.value.includes(reason)
-                        ? field.value.filter((item) => item !== reason)
-                        : [...field.value, reason]
-                      : [reason];
-                    field.onChange(newReasons);
-                  }}
-                />
-              )}
-            />
-          ))}
+
+          <Controller
+            name="reasons"
+            control={control}
+            rules={{
+              validate: (value) => (value && value.length > 0) || 'Please select at least one reason for cancellation.',
+            }}
+            render={({ field, fieldState }) => (
+              <>
+                {MEMBERSHIP_CANCELLATION_REASONS.map((reason) => (
+                  <Checkbox
+                    key={reason}
+                    label={<Text color="white">{reason}</Text>}
+                    value={reason}
+                    checked={Array.isArray(field.value) && field.value.includes(reason)}
+                    onChange={() => {
+                      const newReasons = Array.isArray(field.value)
+                        ? field.value.includes(reason)
+                          ? field.value.filter((item) => item !== reason)
+                          : [...field.value, reason]
+                        : [reason];
+                      field.onChange(newReasons);
+                    }}
+                  />
+                ))}
+                {fieldState.invalid && fieldState.error && (
+                  <Text color="red" size="sm" weight={500}>
+                    {fieldState.error.message}
+                  </Text>
+                )}
+              </>
+            )}
+          />
         </Flex>
 
         <Flex mt="sm" gap="md">
-          <Button onClick={() => modals.closeAll()} fullWidth variant="outline">
+          <Button onClick={onClose} fullWidth variant="outline">
             Close
           </Button>
           <Button type="submit" loading={isCancelPlanLoading} fullWidth>
