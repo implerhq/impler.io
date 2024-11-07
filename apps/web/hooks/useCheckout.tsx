57323257e864e19287a9dd31a4aa381e4c7704bf
Mayur <mayurkaikadi@knovator.in>
2024-11-07 18:18:01 +0530

    feat: Added email as the query key

--- apps/web/hooks/useCheckout.tsx
+++ apps/web/hooks/useCheckout.tsx
@@ -6,15 +6,16 @@ import { useMutation } from '@tanstack/react-query';
 interface UseCheckoutProps {
   planCode?: string;
   couponCode?: string;
+  email?: string;
 }
 
-export function useCheckout({ couponCode, planCode }: UseCheckoutProps) {
+export function useCheckout({ couponCode, planCode, email }: UseCheckoutProps) {
   const {
     mutate: getCheckoutData,
     data: checkoutData,
     isLoading: isCheckoutDataLoading,
   } = useMutation<ICheckoutData, IErrorObject, string | undefined, (string | undefined)[]>(
-    [API_KEYS.CHECKOUT, couponCode, planCode],
+    [API_KEYS.CHECKOUT, couponCode, planCode, email],
     (paymentMethodId) => commonApi(API_KEYS.CHECKOUT as any, { query: { couponCode, planCode, paymentMethodId } })
   );
 
