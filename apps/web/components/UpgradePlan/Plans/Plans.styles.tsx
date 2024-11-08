c1f892564b6d7bc14f6cc84ff162c3770c1d47d4
Mayur <mayurkaikadi@knovator.in>
2024-11-08 12:31:27 +0530

    feat: Removed previously used styling for table

--- apps/web/components/UpgradePlan/Plans/Plans.styles.tsx
+++ apps/web/components/UpgradePlan/Plans/Plans.styles.tsx
@@ -3,35 +3,16 @@ import { MantineTheme, createStyles } from '@mantine/core';
 
 export default createStyles((theme: MantineTheme): Record<string, any> => {
   return {
-    table: {
-      'thead > tr > th': {
-        textAlign: 'center',
-        fontWeight: 'bold',
-        fontSize: theme.fontSizes.lg,
-        textWrap: 'nowrap',
-        padding: theme.spacing.sm,
-      },
-      'tbody > tr td:first-of-type': {
-        fontWeight: 'bold',
-        fontSize: theme.fontSizes.lg,
-        textWrap: 'nowrap',
-        padding: theme.spacing.sm,
-      },
-      'tbody > tr td': {
-        textAlign: 'center',
-      },
-    },
     recommendedBadge: {
       position: 'absolute',
       top: 1,
       left: 0,
-      padding: '20px 10px',
+      padding: theme.spacing.sm,
       borderRadius: 0,
     },
     planName: {
       textAlign: 'center',
-      fontWeight: 500,
-      fontSize: theme.fontSizes.md,
+      fontSize: theme.fontSizes.lg,
     },
     planPrice: {
       textAlign: 'center',
