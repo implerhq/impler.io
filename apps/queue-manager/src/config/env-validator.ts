b4a0ab34c29ae5595e3913aa043d3746d22eb001
bhavik <bhavikchavda@knovator.in>
2025-06-24 11:10:14 +0530

    feat: Added support for azure blob storage in queue-manager

--- apps/queue-manager/src/config/env-validator.ts
+++ apps/queue-manager/src/config/env-validator.ts
@@ -1,12 +1,16 @@
 import { str, ValidatorSpec } from 'envalid';
 import * as envalid from 'envalid';
-import { ENVTypesEnum } from '@impler/shared';
+import { ENVTypesEnum, StorageTypeEnum } from '@impler/shared';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const validators: { [K in keyof any]: ValidatorSpec<any[K]> } = {
   NODE_ENV: str({
     choices: [ENVTypesEnum.DEV, ENVTypesEnum.TEST, ENVTypesEnum.PROD, ENVTypesEnum.CI, ENVTypesEnum.LOCAL],
     default: ENVTypesEnum.LOCAL,
+  }),
+  STORAGE_TYPE: str({
+    choices: [StorageTypeEnum.S3, StorageTypeEnum.AZURE],
+    default: StorageTypeEnum.S3,
   }),
   MONGO_URL: str(),
   RABBITMQ_CONN_URL: str(),
