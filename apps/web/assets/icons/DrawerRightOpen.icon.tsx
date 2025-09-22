78991b334764c5225c2bef7c635a337691069f05
Mayur <iammaya.kakade@gmail.com>
2025-09-22 18:24:58 +0530

    feat: Updated the classes as the jsx based

--- apps/web/assets/icons/DrawerRightOpen.icon.tsx
+++ apps/web/assets/icons/DrawerRightOpen.icon.tsx
@@ -10,7 +10,7 @@ export const DrawerRightOpenIcon = ({ size = 'sm' }: IconType) => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+      <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
         <rect width="18" height="18" x="3" y="3" rx="2" />
         <path d="M15 3v18M8 9l3 3l-3 3" />
       </g>
