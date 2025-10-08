32a900555a3e9e320d1e899c3f5c0b83b17bc912
Mayur <iammaya.kakade@gmail.com>
2025-10-08 17:18:38 +0530

    feat: Added Dynamic Filters

--- libs/shared/src/entities/UserJob/Userjob.interface.ts
+++ libs/shared/src/entities/UserJob/Userjob.interface.ts
@@ -1,3 +1,17 @@
+export enum FilterOperationEnum {
+  CONTAINS = 'contains',
+  EQUALS = 'equals',
+  STARTSWITH = 'startsWith',
+  ENDSWITH = 'endsWith',
+  MATCHES = 'matches',
+}
+
+export interface IFilter {
+  field: string;
+  operation: FilterOperationEnum;
+  value: string;
+}
+
 export interface IUserJob {
   _id: string;
   url: string;
@@ -7,4 +21,5 @@ export interface IUserJob {
   endsOn?: Date;
   headings: string[];
   _templateId: string;
+  filters?: IFilter[];
 }
