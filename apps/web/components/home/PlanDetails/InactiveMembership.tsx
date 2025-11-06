8928b623e3cec7d9ef70ece9d834248b047d574c
Mayur <iammaya.kakade@gmail.com>
2025-11-06 12:34:50 +0530

    feat: Added smooth scroll to show user plans if user do not have any ative plan

--- apps/web/components/home/PlanDetails/InactiveMembership.tsx
+++ apps/web/components/home/PlanDetails/InactiveMembership.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import { useRef } from 'react';
+import { Text, Title, Group, Stack, Divider, Center, Container } from '@mantine/core';
 import Link from 'next/link';
-import { Divider, Stack, Text, Title, Center, Group } from '@mantine/core';
-import { Button } from '@ui/button';
 import { colors, ROUTES } from '@config';
+import { Button } from '@ui/button';
 
 export function InactiveMembership() {
+  const pricingRef = useRef<HTMLDivElement>(null);
+
+  const scrollToPricing = () => {
+    pricingRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
-    <Stack>
-      <Group position="apart" align="center" spacing="sm">
+    <Stack spacing="md">
+      <Group position="apart">
         <Title order={4}>Plan Expiry</Title>
         <Button component={Link} href={ROUTES.TRANSACTIONS} variant="filled" radius="md">
           View all Transactions
@@ -18,14 +24,24 @@ export function InactiveMembership() {
       <Center>
         <Stack w={500} align="center">
           <Text fw="bolder" color={colors.danger} align="center">
-            You don&apos;t have any active membership. Consider choosing an appropriate plan. You can see all
-            transactions{' '}
+            You don&apos;t have any active membership. Consider choosing an{' '}
+            <Text
+              component="span"
+              color={colors.yellow}
+              sx={{ cursor: 'pointer', textDecoration: 'underline' }}
+              onClick={scrollToPricing}
+            >
+              appropriate plan
+            </Text>
+            . You can see all transactions{' '}
             <Text component={Link} href={ROUTES.TRANSACTIONS} color={colors.yellow} td="underline">
               here.
             </Text>
           </Text>
         </Stack>
       </Center>
+
+      <Container ref={pricingRef} />
     </Stack>
   );
 }
