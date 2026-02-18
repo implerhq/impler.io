25a8c00967774cb80896a94969f09b13b9d6aed2
sunil bhadu <sunilchoudharyikka501@gmail.com>
2026-02-18 13:13:58 +0530

    refactor: Replace ImportConfigEnum with TemplateModeEnum in import configuration

--- apps/web/types/import.config.enum.ts
+++
@@ -1,4 +0,0 @@
-export const enum ImportConfigEnum {
-  'AUTOMATED' = 'automated',
-  'MANUAL' = 'manual',
-}
--- apps/web/types/index.ts
+++ apps/web/types/index.ts
@@ -1,3 +1,2 @@
 export * from './component.types';
 export * from './store.types';
-export * from './import.config.enum';
