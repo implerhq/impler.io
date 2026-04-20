7d3db898086276e4bbc5845ae2e7a785de6cadca
Bhavik Chavda <bhavik@knovator.com>
2026-04-20 13:30:18 +0530

    fix: import errors

--- apps/web/config/defineAbilities.ts
+++ apps/web/config/defineAbilities.ts
@@ -1,9 +1,16 @@
 import { AbilityBuilder, createMongoAbility } from '@casl/ability';
 import { UserRolesEnum } from '@impler/shared';
-import { AppAbility, ROLE_BASED_ACCESS } from './constants.config';
+import { ActionsEnum, AppAbility, ROLE_BASED_ACCESS, SubjectsEnum } from './constants.config';
 
-export const defineAbilitiesFor = (role?: string): AppAbility => {
+export const defineAbilitiesFor = (role?: string, isOwner?: boolean): AppAbility => {
   const { can, build } = new AbilityBuilder<AppAbility>(createMongoAbility);
+
+  if (isOwner) {
+    can(ActionsEnum.MANAGE, SubjectsEnum.ALL);
+
+    return build();
+  }
+
   const roleBasedAccess = ROLE_BASED_ACCESS[role as UserRolesEnum] || [];
 
   roleBasedAccess.forEach(({ action, subject }) => {
